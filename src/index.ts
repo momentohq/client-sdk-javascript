fb02fdd5be3442b034c6928a41acb848789525a3
Matt Straathof <matt.straat@gmail.com>
2022-02-02 21:48:55 -0800

    fix: expose errors to customers

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,41 @@ import {SimpleCacheClient} from './SimpleCacheClient';
 import {GetResponse} from './messages/GetResponse';
 import {SetResponse} from './messages/SetResponse';
 import {CacheGetStatus} from './messages/Result';
+import {
+  AlreadyExistsError,
+  AuthenticationError,
+  CancelledError,
+  CacheServiceError,
+  LimitExceededError,
+  ServiceValidationError,
+  InternalServerError,
+  InvalidArgumentError,
+  InvalidJwtError,
+  UnknownServiceError,
+  TimeoutError,
+  BadRequestError,
+  PermissionError,
+  NotFoundError,
+} from './Errors';
 
-export {SimpleCacheClient, GetResponse, SetResponse, CacheGetStatus};
+export {
+  SimpleCacheClient,
+  GetResponse,
+  SetResponse,
+  CacheGetStatus,
+  AlreadyExistsError,
+  AuthenticationError,
+  CancelledError,
+  CacheServiceError,
+  LimitExceededError,
+  ServiceValidationError,
+  InternalServerError,
+  InvalidArgumentError,
+  InvalidJwtError,
+  UnknownServiceError,
+  TimeoutError,
+  BadRequestError,
+  PermissionError,
+  NotFoundError,
+};
 export type {CacheProps};
