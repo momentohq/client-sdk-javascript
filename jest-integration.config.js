ad9877815f6742dbff54907db2a0a013dc36334d
Matt Straathof <matt.straat@gmail.com>
2022-03-22 16:09:04 -0700

    fix: only run 1 int test at a time

--- jest-integration.config.js
+++ jest-integration.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    testEnvironment: 'node',
-    roots: ['<rootDir>/integration'],
-    testMatch: ['**/*.test.ts'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
+  testEnvironment: 'node',
+  roots: ['<rootDir>/integration'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRunner: 'jest-jasmine2',
 };
+gi;
