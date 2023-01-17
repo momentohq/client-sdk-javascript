7e87a68d411cc902219ce3c32bef55c4a93687e1
Vir Ratanasangpunth <vir@momentohq.com>
2023-01-17 14:07:34 -0800

    feat: add unit and integ tests for sets operations

--- integration/simple-cache-client.test.ts
+++ integration/simple-cache-client.test.ts
@@ -17,6 +17,8 @@ import {
   RevokeSigningKey,
   CacheSetFetch,
   SimpleCacheClient,
+  CacheSetAddElements,
+  CacheSetRemoveElements,
 } from '../src';
 import {TextEncoder} from 'util';
 import {SimpleCacheClientProps} from '../src/simple-cache-client-props';
@@ -30,6 +32,9 @@ const cacheClientProps: SimpleCacheClientProps = {
 };
 const INTEGRATION_TEST_CACHE_NAME =
   process.env.TEST_CACHE_NAME || 'js-integration-test-default';
+
+const LOL_BYTE_ARRAY = Uint8Array.of(108, 111, 108);
+const FOO_BYTE_ARRAY = Uint8Array.of(102, 111, 111);
 
 const deleteCacheIfExists = async (
   momento: SimpleCacheClient,
@@ -571,6 +576,221 @@ describe('lists', () => {
 });
 
 describe('Integration Tests for operations on sets datastructure', () => {
+  it('should succeed for addElements with byte arrays happy path', async () => {
+    const setName = v4();
+    const addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY, FOO_BYTE_ARRAY]
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Hit);
+    expect((fetchResponse as CacheSetFetch.Hit).valueSetUint8Array()).toEqual(
+      new Set([LOL_BYTE_ARRAY, FOO_BYTE_ARRAY])
+    );
+  });
+  it('should succeed for addElements with byte arrays happy path with no refresh ttl', async () => {
+    const setName = v4();
+    let addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY, FOO_BYTE_ARRAY],
+      new CollectionTtl(2, false)
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY, FOO_BYTE_ARRAY],
+      new CollectionTtl(10, false)
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    await sleep(2_000);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Miss);
+  });
+  it('should succeed for addElements with byte arrays happy path with refresh ttl', async () => {
+    const setName = v4();
+    let addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY, FOO_BYTE_ARRAY],
+      new CollectionTtl(2, false)
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY, FOO_BYTE_ARRAY],
+      new CollectionTtl(10, true)
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    await sleep(2_000);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Hit);
+    expect((fetchResponse as CacheSetFetch.Hit).valueSetUint8Array()).toEqual(
+      new Set([LOL_BYTE_ARRAY, FOO_BYTE_ARRAY])
+    );
+  });
+  it('should succeed for addElements for string arrays happy path', async () => {
+    const setName = v4();
+    const addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      ['lol', 'foo']
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Hit);
+    expect((fetchResponse as CacheSetFetch.Hit).valueSetString()).toEqual(
+      new Set(['lol', 'foo'])
+    );
+  });
+  it('should succeed for addElements with duplicate elements', async () => {
+    const setName = v4();
+    let addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY, FOO_BYTE_ARRAY]
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+    addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY]
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Hit);
+    expect((fetchResponse as CacheSetFetch.Hit).valueSetUint8Array()).toEqual(
+      new Set([LOL_BYTE_ARRAY, FOO_BYTE_ARRAY])
+    );
+  });
+  it('should succeed for removeElements byte arrays happy path', async () => {
+    const setName = v4();
+    const addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY, FOO_BYTE_ARRAY]
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    const removeResponse = await momento.setRemoveElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [FOO_BYTE_ARRAY]
+    );
+    expect(removeResponse).toBeInstanceOf(CacheSetRemoveElements.Success);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Hit);
+    expect((fetchResponse as CacheSetFetch.Hit).valueSetUint8Array()).toEqual(
+      new Set([LOL_BYTE_ARRAY])
+    );
+  });
+  it('should succeed for removeElements string arrays happy path', async () => {
+    const setName = v4();
+    const addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      ['lol', 'foo']
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    const removeResponse = await momento.setRemoveElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      ['foo']
+    );
+    expect(removeResponse).toBeInstanceOf(CacheSetRemoveElements.Success);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Hit);
+    expect((fetchResponse as CacheSetFetch.Hit).valueSetString()).toEqual(
+      new Set(['lol'])
+    );
+  });
+  it('should succeed for removeElements when the element does not exist', async () => {
+    const setName = v4();
+    const addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY]
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    const removeResponse = await momento.setRemoveElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [FOO_BYTE_ARRAY]
+    );
+    expect(removeResponse).toBeInstanceOf(CacheSetRemoveElements.Success);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Hit);
+    expect((fetchResponse as CacheSetFetch.Hit).valueSetUint8Array()).toEqual(
+      new Set([LOL_BYTE_ARRAY])
+    );
+  });
+  it('should succeed for removeElements when bytes/strings are used together', async () => {
+    const setName = v4();
+    const addResponse = await momento.setAddElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      [LOL_BYTE_ARRAY, FOO_BYTE_ARRAY]
+    );
+    expect(addResponse).toBeInstanceOf(CacheSetAddElements.Success);
+
+    const removeResponse = await momento.setRemoveElements(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName,
+      ['lol']
+    );
+    expect(removeResponse).toBeInstanceOf(CacheSetRemoveElements.Success);
+
+    const fetchResponse = await momento.setFetch(
+      INTEGRATION_TEST_CACHE_NAME,
+      setName
+    );
+    expect(fetchResponse).toBeInstanceOf(CacheSetFetch.Hit);
+    expect((fetchResponse as CacheSetFetch.Hit).valueSetUint8Array()).toEqual(
+      new Set([FOO_BYTE_ARRAY])
+    );
+  });
   it('should return MISS if set does not exist', async () => {
     const noKeyGetResponse = await momento.setFetch(
       INTEGRATION_TEST_CACHE_NAME,
