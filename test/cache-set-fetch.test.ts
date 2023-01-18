5b234b15ca1cc8862859f610d93e01decd54b171
Vir Ratanasangpunth <93940372+virratanasangpunth@users.noreply.github.com>
2023-01-18 15:32:43 -0800

    feat: override toString() for CacheSetFetch.Hit response (#181)

--- test/cache-set-fetch.test.ts
+++ test/cache-set-fetch.test.ts
@@ -8,4 +8,24 @@ describe('CacheSetFetch', () => {
     const hit = new CacheSetFetch.Hit([LOL_BYTE_ARRAY, FOO_BYTE_ARRAY]);
     expect(hit.valueSetString()).toEqual(new Set(['lol', 'foo']));
   });
+  it('should display a correct string for toString()', () => {
+    const hit = new CacheSetFetch.Hit(['foo'].map(v => Buffer.from(v)));
+    expect(hit.toString()).toEqual('Hit: [foo]');
+  });
+  it('should truncate if needed for toString()', () => {
+    const hit = new CacheSetFetch.Hit(
+      [
+        'foooooooooooooooooooooooooooooooooooo',
+        'bar',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+      ].map(v => Buffer.from(v))
+    );
+    expect(hit.toString()).toEqual(
+      'Hit: [fooooooooooooooooooooooooooooooo...,bar,1,2,3,...]'
+    );
+  });
 });
