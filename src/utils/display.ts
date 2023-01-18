d594b6e014a6a4f4b6c1541e730ba2f250f4d191
Vir Ratanasangpunth <93940372+virratanasangpunth@users.noreply.github.com>
2023-01-18 15:24:41 -0800

    chore: refactor common string array display logic for re-use (#180)
    In a subsequent PR I'll need this common logic for set-fetch-response.

--- src/utils/display.ts
+++ src/utils/display.ts
@@ -7,3 +7,26 @@ export function truncateString(value: string, maxLength = 32) {
     return value;
   }
 }
+
+const DISPLAY_SIZE_LIMIT = 5;
+
+function truncateStringArrayToSize(
+  stringArray: string[],
+  length: number
+): string[] {
+  if (stringArray.length <= length) {
+    return stringArray;
+  } else {
+    return stringArray.slice(0, length).concat(['...']);
+  }
+}
+
+export function truncateStringArray(
+  stringArray: string[],
+  length: number = DISPLAY_SIZE_LIMIT
+): string[] {
+  const truncatedStringArray = truncateStringArrayToSize(stringArray, length);
+  return truncatedStringArray.map(s => {
+    return truncateString(s);
+  });
+}
