f5e74fae5be117d19114f217af20d5e8413fccbb
Michael G. Schwern <schwern@pobox.com>
2023-01-18 19:27:07 -0800

    refactor: DRY the simple success responses.

--- src/messages/responses/cache-set.ts
+++ src/messages/responses/cache-set.ts
@@ -1,15 +1,1 @@
-// older versions of node don't have the global util variables https://github.com/nodejs/node/issues/20365
-import {SdkError} from '../../errors/errors';
-import {ResponseBase, ResponseError, ResponseSuccess} from './response-base';
-
-export abstract class Response extends ResponseBase {}
-
-class _Success extends Response {}
-export class Success extends ResponseSuccess(_Success) {}
-
-class _Error extends Response {
-  constructor(public _innerException: SdkError) {
-    super();
-  }
-}
-export class Error extends ResponseError(_Error) {}
+export * from './response-simple-success';
--- src/messages/responses/create-cache.ts
+++ src/messages/responses/create-cache.ts
@@ -1,16 +1,4 @@
-import {SdkError} from '../../errors/errors';
-import {ResponseBase, ResponseError, ResponseSuccess} from './response-base';
-
-export abstract class Response extends ResponseBase {}
-
-class _Success extends Response {}
-export class Success extends ResponseSuccess(_Success) {}
+import {Response} from './response-simple-success';
+export * from './response-simple-success';
 
 export class AlreadyExists extends Response {}
-
-class _Error extends Response {
-  constructor(protected _innerException: SdkError) {
-    super();
-  }
-}
-export class Error extends ResponseError(_Error) {}
