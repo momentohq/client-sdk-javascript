94713c9272bce7aa60bfce5e489b0c5204fb73c2
Chris Price <cprice404@users.noreply.github.com>
2023-02-10 13:07:43 -0800

    chore: improve example code (#281)
    This commit splits out the main example into two files: `basic.ts`
    and `advanced.ts`.  The `basic.ts` is intended to be a quickstart
    so we try to keep it as succint as possible.  The `advanced` example
    adds more features such as listCaches, delete, and an example of how
    to issue concurrent gets.

--- examples/README.md
+++ examples/README.md
@@ -9,37 +9,40 @@ _Read this in other languages_: [日本語](README.ja.md)
 - Node version 14 or higher is required
 - A Momento Auth Token is required, you can generate one using the [Momento CLI](https://github.com/momentohq/momento-cli)
 
-## Running the Example
+To run any of the examples you will need to install the dependencies once first:
 
 ```bash
 npm install
+```
 
+## Running the Basic Example
+
+```bash
 # Run example code
 MOMENTO_AUTH_TOKEN=<YOUR AUTH TOKEN> npm run example
 ```
 
-Example Code: [index.ts](index.ts)
+Example Code: [basic.ts](basic.ts)
+
+## Running the Advanced Example
+
+```bash
+# Run example code
+MOMENTO_AUTH_TOKEN=<YOUR AUTH TOKEN> npm run advanced
+```
+
+Example Code: [advanced.ts](advanced.ts)
 
 ## Running the Dictionary Example
 
 This example demonstrates how to use the dictionary data type.
 
 ```bash
-npm install
-
 # Run example code
 MOMENTO_AUTH_TOKEN=<YOUR AUTH TOKEN> npm run dictionary
 ```
 
 Example Code: [dictionary.ts](dictionary.ts)
-
-## Using the SDK in your projects
-
-### Installation
-
-```bash
-npm install @gomomento/sdk
-```
 
 ## Running the load generator example
 
--- examples/advanced.ts
+++ examples/advanced.ts
@@ -5,52 +5,63 @@ import {
   CacheSet,
   CacheDelete,
   SimpleCacheClient,
-  EnvMomentoTokenProvider,
   Configurations,
   MomentoLoggerFactory,
   DefaultMomentoLoggerFactory,
+  CredentialProvider,
 } from '@gomomento/sdk';
+import {range} from './utils/collections';
 
 const cacheName = 'cache';
 const cacheKey = 'key';
 const cacheValue = 'value';
 
-const credentialsProvider = new EnvMomentoTokenProvider({
-  environmentVariableName: 'MOMENTO_AUTH_TOKEN',
-});
-
+// you can customize your log level or provide your own logger factory to
+// integrate with your favorite logging framework
 const loggerFactory: MomentoLoggerFactory = new DefaultMomentoLoggerFactory();
 
-const defaultTtl = 60;
 const momento = new SimpleCacheClient({
   configuration: Configurations.Laptop.latest(loggerFactory),
-  credentialProvider: credentialsProvider,
-  defaultTtlSeconds: defaultTtl,
+  credentialProvider: CredentialProvider.fromEnvironmentVariable({
+    environmentVariableName: 'MOMENTO_AUTH_TOKEN',
+  }),
+  defaultTtlSeconds: 60,
 });
 
-const main = async () => {
+async function main() {
+  await createCacheExample();
+  await listCachesExample();
+  await setGetDeleteExample();
+  await concurrentGetsExample();
+}
+
+async function createCacheExample() {
   const createCacheResponse = await momento.createCache(cacheName);
   if (createCacheResponse instanceof CreateCache.AlreadyExists) {
     console.log('cache already exists');
   } else if (createCacheResponse instanceof CreateCache.Error) {
     throw createCacheResponse.innerException();
+  } else {
+    throw new Error(`Unexpected response: ${createCacheResponse.toString()}`);
   }
+}
 
+async function listCachesExample() {
   console.log('Listing caches:');
-  let token: string | undefined;
-  do {
-    const listResponse = await momento.listCaches(token);
-    if (listResponse instanceof ListCaches.Error) {
-      console.log(`Error listing caches: ${listResponse.message()}`);
-      break;
-    } else if (listResponse instanceof ListCaches.Success) {
-      listResponse.getCaches().forEach(cacheInfo => {
-        console.log(`${cacheInfo.getName()}`);
-      });
-      token = listResponse.getNextToken();
-    }
-  } while (token !== undefined);
+  const listResponse = await momento.listCaches();
+  if (listResponse instanceof ListCaches.Error) {
+    console.log(`Error listing caches: ${listResponse.message()}`);
+  } else if (listResponse instanceof ListCaches.Success) {
+    console.log('Found caches:');
+    listResponse.getCaches().forEach(cacheInfo => {
+      console.log(`${cacheInfo.getName()}`);
+    });
+  }
+}
 
+async function setGetDeleteExample() {
+  // ttl is an optional field on most write operations, but you can provide it if you
+  // want to override the default ttl that you specified when constructing your client.
   const exampleTtlSeconds = 10;
   console.log(
     `Storing key=${cacheKey}, value=${cacheValue}, ttl=${exampleTtlSeconds}`
@@ -79,7 +90,29 @@ const main = async () => {
   } else if (deleteResponse instanceof CacheDelete.Success) {
     console.log('Deleted key from cache');
   }
-};
+}
+
+async function concurrentGetsExample() {
+  console.log('Saving 10 values to cache');
+  for (let i = 1; i <= 10; i++) {
+    await momento.set(cacheName, `key${i}`, `value${i}`);
+  }
+  console.log('Initiating 10 concurrent gets');
+  const getPromises = range(10).map(i => momento.get(cacheName, `key${i + 1}`));
+  const getResponses = await Promise.all(getPromises);
+  getResponses.forEach((response, index) => {
+    const key = `key${index + 1}`;
+    if (response instanceof CacheGet.Hit) {
+      console.log(
+        `Concurrent get for ${key} returned ${response.valueString()}`
+      );
+    } else {
+      console.log(
+        `Something went wrong with concurrent get for key ${key}: ${response.toString()}`
+      );
+    }
+  });
+}
 
 main()
   .then(() => {
