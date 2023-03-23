d85854c245e1e0327dc341b47c0faa91f59a4bf8
Chris Price <cprice404@users.noreply.github.com>
2023-03-23 12:00:57 -0700

    chore: tweaks to CI jobs to account for symlink refactor (#370)
    * chore: remove package files for common, fix imports via node_modules symlink
    
    This commit removes all of the package.json and ts config files
    from the common dir since we are not treating it as a package, but
    rather just as a directory of files that will be shared via symlink
    between the SDKs.
    
    We also need to do a hacky thing for now in the build script, where
    we symlink the node_modules dir from the sdk into the common dir
    so that imports from the files in common will work.  This won't be
    an issue for the packaged artifact since the symlink won't exist.
    
    In a subsequent commit we still need to add a solution for running
    the shared tests in the common directory.

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -7,6 +7,10 @@ echo "building package: ${1}"
 
 pushd ./packages/${1}
     npm ci
+    # this is gross, but npm will fail to import things
+    # during local builds of files in common if it doesn't
+    # find a node_modules directory there.
+    ln -s $(pwd)/node_modules ../common/node_modules
     npm run build
     npm run lint
 popd
