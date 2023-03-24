4798b2d54e9516964b07fc5c4ee1b0a205c55393
Matt Straathof <matt.straat@gmail.com>
2023-03-24 14:05:06 -0700

    chore: add clarifying comment -f flag for npm ci

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -6,6 +6,10 @@ set -e
 echo "building package: ${1}"
 
 pushd ./packages/${1}
+    # this is a bit hacky, but allows us to build on github consistently. Without the `-f`
+    # we were getting flaky github build errors `npm ERR! code EEXIST`, I believe it has to
+    # do with building the common packages first, and then installing them into each of
+    # the sdks. Ideally we shouldn't have to use the `-f` flag, but using it to unblock us
     npm ci -f
     npm run build
     npm run lint
