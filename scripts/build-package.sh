0a15dc397be9eed460e3e37a8aee52e2fb415f67
Matt Straathof <matt.straat@gmail.com>
2023-03-24 12:43:51 -0700

    fix: build scripts

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -5,6 +5,18 @@ set -e
 
 echo "building package: ${1}"
 
+pushd ./packages/common
+    npm ci
+    npm run build
+    npm run lint
+popd
+
+pushd ./packages/common-integration-tests
+    npm ci
+    npm run build
+    npm run lint
+popd
+
 pushd ./packages/${1}
     npm ci
     npm run build
