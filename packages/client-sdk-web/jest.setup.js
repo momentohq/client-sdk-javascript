23889fadf7a514c218b2fabe972f8773d1c7910e
Matt Straathof <matt.straat@gmail.com>
2023-03-28 18:18:49 -0700

    fix: comments

--- packages/client-sdk-web/jest.setup.js
+++ packages/client-sdk-web/jest.setup.js
@@ -4,7 +4,7 @@ import * as util from 'util'
 // ref: https://github.com/jsdom/jsdom/issues/2524
 
 // Generally speaking, we don't want to have these polyfills here. However, TextEncoder and 
-// TextDecoder are supported by all modern broswers, the only browser that is it not supported
+// TextDecoder are supported by all modern broswers, the only "modern" browser that is it not supported
 // in is IE 11, which is at EOL and retired June 2022 RIP. https://www.lambdatest.com/web-technologies/textencoder. 
 Object.defineProperty(window, 'TextEncoder', {
   writable: true,
