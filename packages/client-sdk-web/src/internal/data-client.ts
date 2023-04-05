d9d7572e8ad6384a3ebaecec011c55e8a367f856
Matt Straathof <matt.straat@gmail.com>
2023-04-05 09:58:04 -0700

    fix: pull out shared unary integration tests

--- packages/client-sdk-web/src/internal/data-client.ts
+++ packages/client-sdk-web/src/internal/data-client.ts
@@ -6,6 +6,9 @@ import {
   UnknownError,
   CacheSet,
   InvalidArgumentError,
+  CacheSetIfNotExists,
+  CacheDelete,
+  CacheIncrement,
 } from '..';
 import {version} from '../../package.json';
 import {Configuration} from '../config/configuration';
@@ -18,7 +21,11 @@ import {Request, UnaryInterceptor, UnaryResponse} from 'grpc-web';
 import {Header, HeaderInterceptorProvider} from './grpc/headers-interceptor';
 import {cacheServiceErrorMapper} from '../errors/cache-service-error-mapper';
 import {
+  _DeleteRequest,
   _GetRequest,
+  _IncrementRequest,
+  _SetIfNotExistsRequest,
+  _SetIfNotExistsResponse,
   _SetRequest,
   ECacheResult,
 } from '@gomomento/generated-types-webtext/dist/cacheclient_pb';
@@ -199,6 +206,185 @@ export class DataClient<
             resolve(new CacheSet.Success());
           } else {
             resolve(new CacheSet.Error(cacheServiceErrorMapper(err)));
+          }
+        }
+      );
+    });
+  }
+
+  public async setIfNotExists(
+    cacheName: string,
+    key: string | Uint8Array,
+    field: string | Uint8Array,
+    ttl?: number
+  ): Promise<CacheSetIfNotExists.Response> {
+    try {
+      validateCacheName(cacheName);
+    } catch (err) {
+      return new CacheSetIfNotExists.Error(normalizeSdkError(err as Error));
+    }
+    if (ttl && ttl < 0) {
+      return new CacheSetIfNotExists.Error(
+        new InvalidArgumentError('ttl must be a positive integer')
+      );
+    }
+    this.logger.trace(
+      `Issuing 'setIfNotExists' request; key: ${key.toString()}, field: ${field.toString()}, ttl: ${
+        ttl?.toString() ?? 'null'
+      }`
+    );
+
+    const result = await this.sendSetIfNotExists(
+      cacheName,
+      this.convert(key),
+      this.convert(field),
+      ttl || this.defaultTtlSeconds * 1000
+    );
+    this.logger.trace(`'setIfNotExists' request result: ${result.toString()}`);
+    return result;
+  }
+
+  private async sendSetIfNotExists(
+    cacheName: string,
+    key: string,
+    field: string,
+    ttlMilliseconds: number
+  ): Promise<CacheSetIfNotExists.Response> {
+    const request = new _SetIfNotExistsRequest();
+    request.setCacheKey(key);
+    request.setCacheBody(field);
+    request.setTtlMilliseconds(ttlMilliseconds);
+    const metadata = this.createMetadata(cacheName);
+
+    return await new Promise(resolve => {
+      this.clientWrapper.setIfNotExists(
+        request,
+        {
+          ...this.authHeaders,
+          ...metadata,
+        },
+        (err, resp) => {
+          if (resp) {
+            switch (resp.getResultCase()) {
+              case _SetIfNotExistsResponse.ResultCase.STORED:
+                resolve(new CacheSetIfNotExists.Stored());
+                break;
+              case _SetIfNotExistsResponse.ResultCase.NOT_STORED:
+                resolve(new CacheSetIfNotExists.NotStored());
+                break;
+              default:
+                resolve(
+                  new CacheGet.Error(
+                    new UnknownError(
+                      'SetIfNotExists responded with an unknown result'
+                    )
+                  )
+                );
+                break;
+            }
+          } else {
+            resolve(
+              new CacheSetIfNotExists.Error(cacheServiceErrorMapper(err))
+            );
+          }
+        }
+      );
+    });
+  }
+
+  public async delete(
+    cacheName: string,
+    key: string | Uint8Array
+  ): Promise<CacheDelete.Response> {
+    try {
+      validateCacheName(cacheName);
+    } catch (err) {
+      return new CacheDelete.Error(normalizeSdkError(err as Error));
+    }
+    this.logger.trace(`Issuing 'delete' request; key: ${key.toString()}`);
+    return await this.sendDelete(cacheName, this.convert(key));
+  }
+
+  private async sendDelete(
+    cacheName: string,
+    key: string
+  ): Promise<CacheDelete.Response> {
+    const request = new _DeleteRequest();
+    request.setCacheKey(key);
+    const metadata = this.createMetadata(cacheName);
+    return await new Promise(resolve => {
+      this.clientWrapper.delete(
+        request,
+        {
+          ...this.authHeaders,
+          ...metadata,
+        },
+        (err, resp) => {
+          if (resp) {
+            resolve(new CacheDelete.Success());
+          } else {
+            resolve(new CacheDelete.Error(cacheServiceErrorMapper(err)));
+          }
+        }
+      );
+    });
+  }
+
+  public async increment(
+    cacheName: string,
+    field: string | Uint8Array,
+    amount = 1,
+    ttl?: number
+  ): Promise<CacheIncrement.Response> {
+    try {
+      validateCacheName(cacheName);
+    } catch (err) {
+      return new CacheIncrement.Error(normalizeSdkError(err as Error));
+    }
+    this.logger.trace(
+      `Issuing 'increment' request; field: ${field.toString()}, amount : ${amount}, ttl: ${
+        ttl?.toString() ?? 'null'
+      }`
+    );
+
+    const result = await this.sendIncrement(
+      cacheName,
+      this.convert(field),
+      amount,
+      ttl || this.defaultTtlSeconds * 1000
+    );
+    this.logger.trace(`'increment' request result: ${result.toString()}`);
+    return result;
+  }
+
+  private async sendIncrement(
+    cacheName: string,
+    field: string,
+    amount = 1,
+    ttlMilliseconds: number
+  ): Promise<CacheIncrement.Response> {
+    const request = new _IncrementRequest();
+    request.setCacheKey(field);
+    request.setAmount(amount);
+    request.setTtlMilliseconds(ttlMilliseconds);
+    const metadata = this.createMetadata(cacheName);
+
+    return await new Promise(resolve => {
+      this.clientWrapper.increment(
+        request,
+        {
+          ...this.authHeaders,
+          ...metadata,
+        },
+        (err, resp) => {
+          if (resp) {
+            if (resp.getValue()) {
+              resolve(new CacheIncrement.Success(resp.getValue()));
+            } else {
+              resolve(new CacheIncrement.Success(0));
+            }
+          } else {
+            resolve(new CacheIncrement.Error(cacheServiceErrorMapper(err)));
           }
         }
       );
--- packages/common-integration-tests/src/index.ts
+++ packages/common-integration-tests/src/index.ts
@@ -1,2 +1,3 @@
 export * from './common-int-test-utils';
 export * from './create-delete-list-cache';
+export * from './get-set-delete';
