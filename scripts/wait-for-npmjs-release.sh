0ec7e9df811272bbbcda1b497d99a59b9406d9ff
Chris Price <chris@momentohq.com>
2023-04-21 15:33:28 -0700

    feat: publish the node.js client library from the new package structure
    This commit updates the publish scripts to publish the node.js
    SDK package in addition to the core package.  We add a script
    to wait for the core package to be available on node.js, then
    update the dependency in the nodejs SDK package to use the newly
    released version of core, then publish the nodejs package.

---
+++ scripts/wait-for-npmjs-release.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -x
+set -e
+
+usage() {
+   echo "Usage: $0 <PACKAGE> <VERSION>"
+}
+
+ROOT_DIR="$(dirname "$0")/.."
+
+PACKAGE=${1}
+if [ "${PACKAGE}" == "" ]
+then
+   echo "Missing required argument: PACKAGE"
+   usage
+   exit 1
+fi
+
+VERSION=${2}
+if [ "${VERSION}" == "" ]
+then
+   echo "Missing required argument: VERSION"
+   usage
+   exit 1
+fi
+
+echo "waiting for npm.js availability for package: ${PACKAGE} with version ${VERSION}"
+
+num_attempts=0
+max_num_attempts=30
+while true; do
+  num_attempts=$((num_attempts+1))
+  package_exists_status_code=$(curl -v -o /dev/null -w "%{http_code}" https://registry.npmjs.org/${PACKAGE}/${VERSION})
+  echo "Package exists status: ${package_exists_status_code}"
+  if [ "${package_exists_status_code}" == "200" ]
+  then
+    echo "Package found!  Done."
+    break
+  fi
+
+  echo "Package not found yet."
+  if [ "${num_attempts}" -ge $max_num_attempts ]
+  then
+    echo "Exceeded maximum number of attempts (${max_num_attempts}); aborting."
+    exit 1
+  fi
+
+  echo "Sleeping 10 seconds before retrying."
+  sleep 10
+done
