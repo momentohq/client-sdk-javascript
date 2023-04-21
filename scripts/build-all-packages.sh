6280b43f006d4502b22f24ea7bdc13dc725115bd
Pete Gautier <pete@momentohq.com>
2023-04-21 09:24:31 -0700

    chore: rename common package to core

--- scripts/build-all-packages.sh
+++ scripts/build-all-packages.sh
@@ -7,7 +7,7 @@ ROOT_DIR="$(dirname "$0")/.."
 
 echo "dev building web sdk"
 
-${ROOT_DIR}/scripts/build-package.sh "common"
+${ROOT_DIR}/scripts/build-package.sh "core"
 ${ROOT_DIR}/scripts/build-package.sh "common-integration-tests"
 ${ROOT_DIR}/scripts/build-package.sh "client-sdk-nodejs"
 ${ROOT_DIR}/scripts/build-package.sh "client-sdk-web"
--- scripts/build-nodejs-sdk.sh
+++ scripts/build-nodejs-sdk.sh
@@ -5,5 +5,5 @@ set -e
 
 echo "dev building web sdk"
 
-./scripts/build-package.sh "common"
+./scripts/build-package.sh "core"
 ./scripts/build-package.sh "client-sdk-nodejs"
