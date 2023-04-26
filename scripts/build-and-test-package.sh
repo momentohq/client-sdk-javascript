e1a7d66af1812e3534ec528ffbf0020bf40cdd55
Pete Gautier <pete@momentohq.com>
2023-04-26 15:37:16 -0700

    fix: restore test behavior

--- scripts/build-and-test-package.sh
+++ scripts/build-and-test-package.sh
@@ -11,5 +11,6 @@ echo "building and testing package: ${1}"
 ${ROOT_DIR}/scripts/build-package.sh ${1}
 
 pushd ${ROOT_DIR}/packages/${1}
-    npx jest --maxWorkers 1
+    npm run unit-test
+    npm run integration-test
 popd
