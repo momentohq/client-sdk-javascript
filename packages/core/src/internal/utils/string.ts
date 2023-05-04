e213e5eb8b4d127c1df906e6b0e790feadf51621
Erika Tharp <eem@momentohq.com>
2023-05-04 15:12:29 -0700

    chore: remove comment about deprecated method (#490)

--- packages/core/src/internal/utils/string.ts
+++ packages/core/src/internal/utils/string.ts
@@ -1,13 +1,3 @@
-/* Attention:
-before merging this console branch back into main, we need to
-fix how we are encoding/decoding base64 strings. The functions atob and btoa
-are deprecated in the node environment, but Buffer does not exist in a browser. There
-are a few ways we can handle this.
-
-1. have some way to determine an isNode() boolean flag
-2. pull these functions out into their respective sdks
-3. maybe some 3rd party library can help? */
-
 import {Buffer} from 'buffer/index';
 
 export const decodeFromBase64 = (base64: string) => {
