8ac92c5c05e993302034580974ebb27f4135cfeb
Chris Price <chris@momentohq.com>
2023-06-01 11:36:14 -0700

    chore: add example for advanced logging scenarios

---
+++ examples/nodejs/docs-advanced-logging-example.ts
@@ -0,0 +1,30 @@
+import {CacheClient, Configurations, CredentialProvider} from '@gomomento/sdk';
+import {PinoMomentoLoggerFactory} from './doc-example-files/pino-logger';
+
+function example_observability_CreateCacheClientWithPinoLogger(): CacheClient {
+  return new CacheClient({
+    configuration: Configurations.Laptop.v1(
+      new PinoMomentoLoggerFactory({
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+          },
+        },
+      })
+    ),
+    credentialProvider: CredentialProvider.fromEnvironmentVariable({environmentVariableName: 'MOMENTO_AUTH_TOKEN'}),
+    defaultTtlSeconds: 60,
+  });
+}
+
+async function main() {
+  const cacheClient = example_observability_CreateCacheClientWithPinoLogger();
+  await cacheClient.createCache('test-cache');
+  await cacheClient.set('test-cache', 'foo', 'FOO!');
+  await cacheClient.get('test-cache', 'foo');
+}
+
+main().catch(e => {
+  throw e;
+});
