269572b0411418f1546d4950fb7e021856decb1a
Matt Straathof <matt.straat@gmail.com>
2023-06-02 16:04:33 -0700

    fix: broken readme links

--- examples/nodejs/basic/observability.ts
+++ examples/nodejs/basic/observability.ts
@@ -7,8 +7,10 @@ example_observability_setupMetrics();
 
 import {CacheGet, CreateCache, CacheSet, CacheClient, Configurations, CredentialProvider} from '@gomomento/sdk';
 import {ExampleMetricMiddleware} from './doc-example-files/example-metric-middleware';
+import {uuid} from 'uuidv4';
 
 async function main() {
+  const cache = 'cache';
   const momento = new CacheClient({
     configuration: Configurations.Laptop.v1()
       // This is where the middleware that captures the request count metric is added.
@@ -20,28 +22,32 @@ async function main() {
   });
 
   console.log("Creating cache 'cache'");
-  const createCacheResponse = await momento.createCache('cache');
+  const createCacheResponse = await momento.createCache(cache);
   if (createCacheResponse instanceof CreateCache.AlreadyExists) {
     console.log('cache already exists');
   } else if (createCacheResponse instanceof CreateCache.Error) {
     throw createCacheResponse.innerException();
   }
 
-  console.log('Storing key=foo, value=FOO');
-  const setResponse = await momento.set('cache', 'foo', 'FOO');
-  if (setResponse instanceof CacheSet.Success) {
-    console.log('Key stored successfully!');
-  } else {
-    console.log(`Error setting key: ${setResponse.toString()}`);
-  }
+  for (let i = 0; i < 100; i++) {
+    const key = uuid();
+    const value = uuid();
+    console.log(`${i}: Storing key=${key}, value=${value}`);
+    const setResponse = await momento.set(cache, key, value);
+    if (setResponse instanceof CacheSet.Success) {
+      console.log('Key stored successfully!');
+    } else {
+      console.log(`Error setting key: ${setResponse.toString()}`);
+    }
 
-  const getResponse = await momento.get('cache', 'foo');
-  if (getResponse instanceof CacheGet.Hit) {
-    console.log(`cache hit: ${getResponse.valueString()}`);
-  } else if (getResponse instanceof CacheGet.Miss) {
-    console.log('cache miss');
-  } else if (getResponse instanceof CacheGet.Error) {
-    console.log(`Error: ${getResponse.message()}`);
+    const getResponse = await momento.get(cache, key);
+    if (getResponse instanceof CacheGet.Hit) {
+      console.log(`cache hit: ${getResponse.valueString()}`);
+    } else if (getResponse instanceof CacheGet.Miss) {
+      console.log('cache miss');
+    } else if (getResponse instanceof CacheGet.Error) {
+      console.log(`Error: ${getResponse.message()}`);
+    }
   }
 }
 
