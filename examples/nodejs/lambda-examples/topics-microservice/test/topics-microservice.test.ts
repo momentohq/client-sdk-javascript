e1f283d7e16f47847f9ea862716ba21c7b8d6807
Kirk Kirkconnell <synchrophoto@gmail.com>
2023-06-13 16:58:32 -0700

    after cdk init

---
+++ examples/nodejs/lambda-examples/topics-microservice/test/topics-microservice.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TopicsMicroservice from '../lib/topics-microservice-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/topics-microservice-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TopicsMicroservice.TopicsMicroserviceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
