5e612f233da601c3bd81e516a7d121cd77fe21e3
Ben Kugler <124546494+benkug-momento@users.noreply.github.com>
2023-06-14 13:37:53 -0700

    only enable auth test for rn

--- scripts/test-auth-packages.sh
+++ scripts/test-auth-packages.sh
@@ -7,6 +7,11 @@ ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/.. &> /dev/null && pwd )
 
 echo "running auth testing"
 
+${ROOT_DIR}/scripts/build-package.sh "core"
+${ROOT_DIR}/scripts/build-package.sh "common-integration-tests"
+${ROOT_DIR}/scripts/build-package.sh "client-sdk-nodejs"
+${ROOT_DIR}/scripts/build-package.sh "client-sdk-web"
+
 pushd ${ROOT_DIR}/packages/client-sdk-web
     npm run integration-test-auth
 popd
