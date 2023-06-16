bce0859e1ff3cce3c8ceff667e617091031a9f6e
rishtigupta <rishti@momentohq.com>
2023-06-16 15:34:16 -0700

    feat: create a dockerfile to run topics-loadgen

---
+++ examples/web/topics/infrastructure/lib/topics-loadgen-stack.ts
@@ -0,0 +1,58 @@
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
+import {Platform} from 'aws-cdk-lib/aws-ecr-assets';
+import {aws_s3} from 'aws-cdk-lib';
+
+export class TopicsLoadGenStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'TopicsLoadGenVpc', {
+      maxAzs: 1,
+    });
+
+    const cluster = new ecs.Cluster(this, 'TopicsLoadGenCluster', {
+      clusterName: 'topics-loadgen-test-cluster',
+      vpc: vpc,
+    });
+
+    const topicsLoadGenTestImage = ecs.ContainerImage.fromAsset(path.join(__dirname, '../../src'), {
+      platform: Platform.LINUX_AMD64,
+    });
+
+    const topicsLoadGenTestBucket = new aws_s3.Bucket(this, 'topics-loadgen-test-bucket', {});
+
+    const topicsLoadGenTaskRole = new iam.Role(this, 'topics-loadgen-test-task-role', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+
+    topicsLoadGenTestBucket.grantWrite(topicsLoadGenTaskRole);
+
+    const loadGenTestDefinition = new ecs.FargateTaskDefinition(this, 'topics-loadgen-test-task-def', {
+      memoryLimitMiB: 1024,
+      taskRole: topicsLoadGenTaskRole,
+      cpu: 512,
+      runtimePlatform: {
+        operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
+        cpuArchitecture: ecs.CpuArchitecture.X86_64,
+      },
+      family: 'topics-loadgen-test',
+    });
+
+    const topicsLoadGenContainerDefinition = new ecs.ContainerDefinition(
+      this,
+      'topics-loadgen-test-container-definition',
+      {
+        taskDefinition: loadGenTestDefinition,
+        image: topicsLoadGenTestImage,
+        logging: ecs.LogDriver.awsLogs({
+          streamPrefix: 'topics-loadgen-test-load-driver',
+        }),
+      }
+    );
+  }
+}
