b782282352fd2c0a3a374852fc6c20eb85b68cbd
Chris Price <chris@momentohq.com>
2023-06-22 08:30:59 -0700

    chore: additional tests for itemGetTtl

--- packages/common-integration-tests/src/item-get-ttl.ts
+++ packages/common-integration-tests/src/item-get-ttl.ts
@@ -5,7 +5,8 @@ import {
   expectWithMessage,
 } from './common-int-test-utils';
 import {ICacheClient} from '@gomomento/sdk-core/dist/src/internal/clients/cache';
-import {CacheItemGetTtl} from '@gomomento/sdk-core';
+import {CacheItemGetTtl, CollectionTtl} from '@gomomento/sdk-core';
+import {delay} from './auth-client';
 export function runItemGetTtlTest(
   Momento: ICacheClient,
   IntegrationTestCacheName: string
@@ -15,7 +16,7 @@ export function runItemGetTtlTest(
       return Momento.itemGetTtl(props.cacheName, v4());
     });
 
-    it('should get item ttl remaining', async () => {
+    it('should get item ttl remaining for a scalar', async () => {
       const cacheKey = v4();
       const cacheValue = v4();
       await Momento.set(IntegrationTestCacheName, cacheKey, cacheValue, {
@@ -45,6 +46,71 @@ export function runItemGetTtlTest(
       hitResult = itemGetTtlResponse as CacheItemGetTtl.Hit;
       expect(hitResult.remainingTtlMillis()).toBeLessThan(10000);
       expect(hitResult.remainingTtlMillis()).toBeGreaterThan(9000);
+    });
+
+    it('should get item ttl remaining for a dictionary', async () => {
+      const cacheKey = v4();
+      const cacheValue = {foo: v4()};
+      await Momento.dictionarySetFields(
+        IntegrationTestCacheName,
+        cacheKey,
+        cacheValue,
+        {
+          ttl: CollectionTtl.of(10),
+        }
+      );
+
+      // string cache key
+      let itemGetTtlResponse = await Momento.itemGetTtl(
+        IntegrationTestCacheName,
+        cacheKey
+      );
+      expectWithMessage(() => {
+        expect(itemGetTtlResponse).toBeInstanceOf(CacheItemGetTtl.Hit);
+      }, `expected HIT but got ${itemGetTtlResponse.toString()}`);
+      let hitResult = itemGetTtlResponse as CacheItemGetTtl.Hit;
+      expect(hitResult.remainingTtlMillis()).toBeLessThan(10000);
+      expect(hitResult.remainingTtlMillis()).toBeGreaterThan(9000);
+    });
+
+    it('should return a miss for a non-existent key', async () => {
+      const cacheKey = v4();
+      // string cache key
+      const itemGetTtlResponse = await Momento.itemGetTtl(
+        IntegrationTestCacheName,
+        cacheKey
+      );
+      expectWithMessage(() => {
+        expect(itemGetTtlResponse).toBeInstanceOf(CacheItemGetTtl.Miss);
+      }, `expected MISS but got ${itemGetTtlResponse.toString()}`);
+    });
+
+    it('should return a miss for an expired key', async () => {
+      const cacheKey = v4();
+      const cacheValue = v4();
+      await Momento.set(IntegrationTestCacheName, cacheKey, cacheValue, {
+        ttl: 0.5,
+      });
+
+      // string cache key
+      let itemGetTtlResponse = await Momento.itemGetTtl(
+        IntegrationTestCacheName,
+        cacheKey
+      );
+      expectWithMessage(() => {
+        expect(itemGetTtlResponse).toBeInstanceOf(CacheItemGetTtl.Hit);
+      }, `expected HIT but got ${itemGetTtlResponse.toString()}`);
+
+      await delay(1000);
+
+      // byte array cache key
+      itemGetTtlResponse = await Momento.itemGetTtl(
+        IntegrationTestCacheName,
+        cacheKey
+      );
+      expectWithMessage(() => {
+        expect(itemGetTtlResponse).toBeInstanceOf(CacheItemGetTtl.Miss);
+      }, `expected MISS but got ${itemGetTtlResponse.toString()}`);
     });
   });
 }
