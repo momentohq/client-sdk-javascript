5bd426c255db22f6e7253a8fc7e3da02488a6995
Kirk Kirkconnell <synchrophoto@gmail.com>
2023-06-28 08:23:24 -0700

    Tuning things up

--- examples/nodejs/lambda-examples/topics-microservice/resources/topics.ts
+++ examples/nodejs/lambda-examples/topics-microservice/resources/topics.ts
@@ -1,9 +1,12 @@
-import { APIGatewayEvent, Handler } from "aws-lambda";
+import { APIGatewayEvent } from "aws-lambda";
 import {CreateTopicClient} from './libMomentoClient';
 import {PublishingWrapper} from "./libMomento";
 // Import configurations for region and secrets name info.
 // @ts-ignore
 import config from './config.json';
+import {TopicClient} from "@gomomento/sdk";
+
+let client: TopicClient | undefined;
 
 const buildResponseBody = (status: number, body: string, headers = {}) => {
   return {
@@ -15,14 +18,10 @@ const buildResponseBody = (status: number, body: string, headers = {}) => {
 
 export const handler = async (event: APIGatewayEvent): Promise<any> => {
   try {
-
     const body = event.body ? JSON.parse(event.body) : {};
 
-    console.log('This is the body topic name: ', body.topicName);
-    console.log('This is the body topic value: ', body.topicValue);
-
-    // Get the Momento Topics client.
-    let client = await CreateTopicClient();
+    // Only go get the Momento Topics client if it doesn't exist yet. If it does, just reuse the existing one to speed up execution.
+    client = client || await CreateTopicClient();
 
     // Init the publishing wrapper class.
     const wrapper = new PublishingWrapper({
@@ -30,26 +29,15 @@ export const handler = async (event: APIGatewayEvent): Promise<any> => {
       cacheName: config.cacheName, //get the cache from the config.json.
       topicName: body.topicName // The topic name from the query string.
     });
-    try {
-      // Call the publishItem function
-      const ret = await wrapper.publishItem(body.topicValue);
-      console.log(ret);
-      return {
-        statusCode: 200,
-        headers: {},
-        body: ret,
-      };
-    } catch (err){
-      console.error(err);
-      return {
-        statusCode: 500,
-        headers: {},
-        body: "Failed to set topic value.",
-      };
-    }
+
+    // Call the publishItem function
+    const ret = await wrapper.publishItem(body.topicValue);
+
+    return buildResponseBody( 200, ret);
+
   } catch (err) {
     console.error(err);
-
-    return buildResponseBody(500, "Unknown server error from topics.ts");
+    const errorMsg = err instanceof PublishingWrapper ? 'Failed to set topic value. ' : 'Unknown server error from topics.ts, ';
+    return buildResponseBody(500, errorMsg + err);
   }
 };
