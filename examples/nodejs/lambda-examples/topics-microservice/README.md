3fec7ebe046fb157f7a04c834125afc4226e425d
Kirk Kirkconnell <synchrophoto@gmail.com>
2023-06-30 10:00:47 -0700

    chore: Example microservice for publishing to Momento Topics. (#603)
    * initial push.
    
    * Fixed conflicting default word in function definition.
    
    * building out more.
    
    * after cdk init
    
    * ongoing commit
    
    * chore: latest phase of dev
    
    * fixing config.json
    
    * continuing the slog, but making strong headway.
    
    * more
    
    * Fix: fixed the deployment of API gateway and using the body values in the Lambda Function.
    
    * Solved a problem I created
    
    * removing code files that were examples.
    
    * Tuning things up
    
    * adding content to README
    
    * Changing code from using config.json over to using env vars.
    
    * fixed env var names.
    
    * changing RAM usage.
    
    * Fixing things after peer review.
    
    * Update README.md
    
    * trying to fix package issues.
    
    * fixed package.json to remove v1 deps.
    
    * removing jest as it is causing problems and I am not really using it anyhow.
    
    * adding CI stuff
    
    * Update build.yml
    
    * cleaning up more.
    
    * chore: create package-lock.json via node 16 for backward compat
    
    * chore: switch examples build to ubuntu because they build docker
    
    * WIP: switch jobs back to macos because of timeout issues
    
    ---------
    
    Co-authored-by: Chris Price <chris@momentohq.com>

---
+++ examples/nodejs/lambda-examples/topics-microservice/README.md
@@ -0,0 +1,15 @@
+# Welcome to an example microservice using Momento Topics
+
+This example create an API Gateway configuration and deploys an AWS Lambda function to publish data to a topic in Momento Topics.
+
+## Prerequisites
+- An AWS account
+- A Momento auth token from the [Momento console](https://console.gomomento.com).
+- The Momento auth token [stored in AWS Secrets Manager](https://docs.momentohq.com/develop/integrations/aws-secrets-manager) as text.
+- AWS CDK command line tool installed (optional as you can run CDK via npm with `npm run cdk`)
+- Node.js installed
+
+## Useful commands
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk diff`        compare deployed stack with current state
+* `cdk synth`       emits the synthesized CloudFormation template
---
+++ examples/nodejs/lambda-examples/topics-microservice/lib/topics-microservice-stack.ts
@@ -0,0 +1,91 @@
+import {App, aws_secretsmanager, Duration, Stack} from 'aws-cdk-lib';
+import {RestApi, LambdaIntegration, Model, JsonSchemaType, RequestValidator} from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+
+export class TopicsMicroserviceStack extends Stack {
+  constructor(app: App, id: string) {
+    super(app, id);
+
+    // These three value you will need to change for your own deployment
+    const region = "us-west-2";
+    const secretName = "Momento_Auth_Token";
+    const cachName = "default-cache";
+
+    // Set up Lambda function configurations.
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      environment: {
+        "RUNTIME": "AWS",
+        "REGION": region,
+        "SECRETNAME": secretName,
+        "CACHENAME": cachName
+      },
+      bundling: {
+        externalModules: [
+          'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
+        ],
+      },
+      depsLockFilePath: join(__dirname, '..', 'package-lock.json'),
+      runtime: Runtime.NODEJS_18_X,
+      memorySize: 1024, // Increase memory to help with response times
+      timeout: Duration.seconds(10) // Make timeout longer for bootstrap api
+    }
+
+    // Create a Lambda function for handling service requests
+    const serviceLambda = new NodejsFunction(this, 'ServiceLambda', {
+      entry: join(__dirname, '../resources', 'topics.ts'),
+      ...nodeJsFunctionProps,
+    });
+
+    // Get the ARN for the existing secret the Lambda function will be using.
+    let secret = aws_secretsmanager.Secret.fromSecretNameV2(
+      this,
+      "Momento_Authentication_Token",
+      secretName
+    );
+
+    // Grant read access for that secret to the Lambda function.
+    secret.grantRead(serviceLambda);
+
+
+
+    // Define the API Gateaway integration
+    const api = new RestApi(this, "topics-api", {
+      restApiName: "Topics Service",
+      description: "This service takes in data to write to Momento Topics."
+    });
+
+    // Define the API Gateway request validator
+    const requestValidator = new RequestValidator(this, 'RequestValidator', {
+      restApi: api,
+      validateRequestBody: true,
+      validateRequestParameters: false,
+    });
+
+    const svcLambdaIntegration = new LambdaIntegration(serviceLambda);
+
+    const topicsModel: Model = api.addModel('TopicsModel', {
+      schema: {
+        type: JsonSchemaType.OBJECT,
+        properties: {
+          topicValue: {
+            type: JsonSchemaType.STRING
+          },
+          topicName: {
+            type: JsonSchemaType.STRING
+          }
+        },
+        required: ['topicName', 'topicValue']
+      },
+      contentType: 'application/json'
+    });
+
+    const topicsResource = api.root.addResource('topics');
+
+    topicsResource.addMethod("POST", svcLambdaIntegration, {
+      requestModels: { 'application/json': topicsModel },
+      requestValidator: requestValidator,
+    });
+  }
+}
