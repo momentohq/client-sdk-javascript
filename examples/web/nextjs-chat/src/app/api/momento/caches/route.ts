93eea50e961c60af66703ab1f9fb1ab0d0b6eaf6
Matt Straathof <matt.straat@gmail.com>
2023-07-07 16:23:33 -0700

    chore: fix lint

--- examples/web/nextjs-chat/src/app/api/momento/caches/route.ts
+++ examples/web/nextjs-chat/src/app/api/momento/caches/route.ts
@@ -1,25 +1,25 @@
 import {
-    CacheClient,
-    Configurations,
-    CredentialProvider,
-    ListCaches,
+  CacheClient,
+  Configurations,
+  CredentialProvider,
+  ListCaches,
 } from "@gomomento/sdk";
 
 const cacheClient = new CacheClient({
-    credentialProvider: CredentialProvider.fromString({
-        authToken: process.env.MOMENTO_AUTH_TOKEN,
-    }),
-    defaultTtlSeconds: 60,
-    configuration: Configurations.Laptop.v1()
-})
+  credentialProvider: CredentialProvider.fromString({
+    authToken: process.env.MOMENTO_AUTH_TOKEN,
+  }),
+  defaultTtlSeconds: 60,
+  configuration: Configurations.Laptop.v1(),
+});
 
-export async function GET(request: Request) {
-    const listCachesRep = await cacheClient.listCaches();
-    if (listCachesRep instanceof ListCaches.Success) {
-        const listOfCacheNames = listCachesRep.getCaches().map(c => c.getName());
-        return new Response(JSON.stringify(listOfCacheNames))
-    } else if (listCachesRep instanceof  ListCaches.Error) {
-        throw new Error(listCachesRep.message())
-    }
-    throw new Error("Unable to list caches from momento")
+export async function GET(_request: Request) {
+  const listCachesRep = await cacheClient.listCaches();
+  if (listCachesRep instanceof ListCaches.Success) {
+    const listOfCacheNames = listCachesRep.getCaches().map((c) => c.getName());
+    return new Response(JSON.stringify(listOfCacheNames));
+  } else if (listCachesRep instanceof ListCaches.Error) {
+    throw new Error(listCachesRep.message());
+  }
+  throw new Error("Unable to list caches from momento");
 }
