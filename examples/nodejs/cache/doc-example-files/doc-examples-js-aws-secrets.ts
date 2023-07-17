f82c633281857dc2de73fdfa3309fb42148eb3e0
anitarua <anita@momentohq.com>
2023-07-17 15:20:01 -0700

    linted and formatted the example file

--- examples/nodejs/cache/doc-example-files/doc-examples-js-aws-secrets.ts
+++ examples/nodejs/cache/doc-example-files/doc-examples-js-aws-secrets.ts
@@ -1,64 +1,43 @@
-import {
-  CacheClient,
-  Configurations,
-  CredentialProvider,
-  CacheGet, 
-  CacheSet, 
-  ListCaches, 
-  CreateCache,
-} from '@gomomento/sdk';
+import {CacheClient, Configurations, CredentialProvider, CacheGet, CacheSet, CreateCache} from '@gomomento/sdk';
 
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-  GetSecretValueCommandOutput,
-} from '@aws-sdk/client-secrets-manager';
+import {SecretsManagerClient, GetSecretValueCommand} from '@aws-sdk/client-secrets-manager';
 
 /* A function that gets the Momento_Auth_Token stored in AWS Secrets Manager.
 The secret was stored as a plaintext format in Secrets Manager to avoid parsing JSON.
 You don't have to store the Momento auth token in something like AWS Secrets Manager,
 but it is best practice. You could pass the Momento auth token in from an environment variable.
 */
-async function getToken(
-  secretName: string,
-  regionName: string = "us-west-2"
-  ): Promise<string> {
-    try {
-      const client = new SecretsManagerClient({ region: regionName});
-      const response: GetSecretValueCommandOutput = await client.send(
-        new GetSecretValueCommand({
-          SecretId: secretName,
-          VersionStage: "AWSCURRENT",
-        })
-      );
-      return response.SecretString || '';
-    } catch (error) {
-      console.error(`Error fetching secret value for "${secretName}":`, (error as Error).message);
-      // For a list of exceptions thrown, see
-      // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-      throw error;
-    }
+async function getToken(secretName: string, regionName = 'us-west-2'): Promise<string> {
+  try {
+    const client = new SecretsManagerClient({region: regionName});
+    const response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secretName,
+        VersionStage: 'AWSCURRENT',
+      })
+    );
+    return response.SecretString || '';
+  } catch (error) {
+    console.error(`Error fetching secret value for "${secretName}":`, (error as Error).message);
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
   }
+}
 
 // Function that calls to the GetToken function and then gets a client connection
 // object from Momento Cache and returns that for later use.
-async function createCacheClient(
-  ttl:number = 600,
-  tokenName:string = "MOMENTO_AUTH_TOKEN", 
-  ): Promise<CacheClient> {
+async function createCacheClient(ttl = 600, tokenName = 'MOMENTO_AUTH_TOKEN'): Promise<CacheClient> {
   const token: string = await getToken(tokenName);
-    return new CacheClient({
-        configuration: Configurations.Laptop.v1(),
-        credentialProvider: CredentialProvider.fromString({ "authToken" : token }),
-        defaultTtlSeconds: ttl,
-    });
+  return new CacheClient({
+    configuration: Configurations.Laptop.v1(),
+    credentialProvider: CredentialProvider.fromString({authToken: token}),
+    defaultTtlSeconds: ttl,
+  });
 }
 
 // A function to create a cache
-async function createCache(
-  client: CacheClient, 
-  cacheName: string, 
-) {
+async function createCache(client: CacheClient, cacheName: string) {
   const result = await client.createCache(cacheName);
   if (result instanceof CreateCache.Success) {
     console.log(`Cache '${cacheName}' created`);
@@ -72,35 +51,27 @@ async function createCache(
 }
 
 // A function to write to the cache
-async function writeToCache(
-  client: CacheClient, 
-  cacheName: string, 
-  key: string, 
-  data: string
-  ) {
-    const setResponse = await client.set(cacheName, key, data);
-    if (setResponse instanceof CacheSet.Success) {
-      console.log('Key stored successfully!');
-    } else if (setResponse instanceof CacheSet.Error) {
-      console.log('Error setting key: ', setResponse.toString());
-    } else {
-      console.log('Some other error: ', setResponse.toString());
-    }
+async function writeToCache(client: CacheClient, cacheName: string, key: string, data: string) {
+  const setResponse = await client.set(cacheName, key, data);
+  if (setResponse instanceof CacheSet.Success) {
+    console.log('Key stored successfully!');
+  } else if (setResponse instanceof CacheSet.Error) {
+    console.log('Error setting key: ', setResponse.toString());
+  } else {
+    console.log('Some other error: ', setResponse.toString());
   }
+}
 
 // A function to read scalar items from the cache
-async function readFromCache(
-  client: CacheClient, 
-  cacheName: string, 
-  key: string,) {
-    const fileResponse = await client.get(cacheName, key);
-    if (fileResponse instanceof CacheGet.Hit) {
-        console.log('Cache hit: ', fileResponse.valueString());
-    } else if (fileResponse instanceof CacheGet.Miss) {
-        console.log('Cache miss');
-    } else if (fileResponse instanceof CacheGet.Error) {
-        console.log('Error: ', fileResponse.message());
-    }
+async function readFromCache(client: CacheClient, cacheName: string, key: string) {
+  const fileResponse = await client.get(cacheName, key);
+  if (fileResponse instanceof CacheGet.Hit) {
+    console.log('Cache hit: ', fileResponse.valueString());
+  } else if (fileResponse instanceof CacheGet.Miss) {
+    console.log('Cache miss');
+  } else if (fileResponse instanceof CacheGet.Error) {
+    console.log('Error: ', fileResponse.message());
+  }
 }
 
 // Call the various functions
@@ -110,8 +81,10 @@ async function run() {
   const cacheClient = await createCacheClient();
 
   await createCache(cacheClient, CACHE_NAME);
-  await writeToCache(cacheClient, CACHE_NAME, "code", "12345");
-  await readFromCache(cacheClient, CACHE_NAME, "code");
+  await writeToCache(cacheClient, CACHE_NAME, 'code', '12345');
+  await readFromCache(cacheClient, CACHE_NAME, 'code');
 }
 
-run();
+run().catch(e => {
+  throw e;
+});
