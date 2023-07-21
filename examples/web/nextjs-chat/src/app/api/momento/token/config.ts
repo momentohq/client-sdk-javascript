42920ef421d4a6fc64101380ea2c3ab4aa73d5e0
anitarua <anita@momentohq.com>
2023-07-21 14:38:06 -0700

    feat: allow configurable tokens in nextjs app example

---
+++ examples/web/nextjs-chat/src/app/api/momento/token/config.ts
@@ -0,0 +1,52 @@
+import {
+  AllDataReadWrite,
+  ExpiresIn,
+  TopicRole, 
+  CacheRole, 
+  TokenScope, 
+  AllTopics, 
+  AllCaches
+} from "@gomomento/sdk";
+
+/**
+ * First, set the scope of permissions for your tokens. 
+ * 
+ * AllDataReadWrite provides read and write permissions to all of your caches:
+ *    export const tokenPermissions: TokenScope =  AllDataReadWrite;
+ * 
+ * You may also provide a bespoke list of permissions for each cache and topic that you have:
+ *    export const tokenPermissions: TokenScope =  {
+ *      permissions: [
+ *        {
+ *          role: CacheRole.ReadWrite | CacheRole.ReadOnly, 
+ *          cache: AllCaches | "your-cache-name"
+ *        },
+ *        {
+ *          role: TopicRole.PublishSubscribe | TopicRole.SubscribeOnly, 
+ *          cache: AllCaches | "your-cache-name",
+ *          topic: AllTopics | "your-topic-name"
+ *        }
+ *      ]
+ *    };
+ * 
+ * More information here: https://docs.momentohq.com/develop/api-reference/auth-tokens#tokenscope-objects
+ */
+export const tokenPermissions: TokenScope = {
+  permissions: [
+    {
+      role: CacheRole.ReadWrite,
+      cache: "default-cache"
+    },
+    {
+      role: TopicRole.PublishSubscribe, 
+      cache: "default-cache",
+      topic: AllTopics
+    }
+]};
+
+/**
+ * Second, set the TTL for your tokens in terms of seconds, minutes, hours,  
+ * days, or using epoch format. You may also set tokens to never expire.
+ * More information here: https://docs.momentohq.com/develop/api-reference/auth-tokens#generateauthtoken-api
+ */
+export const tokenExpiresIn: ExpiresIn = ExpiresIn.minutes(5);
