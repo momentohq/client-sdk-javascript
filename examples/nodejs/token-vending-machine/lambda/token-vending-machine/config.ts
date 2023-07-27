e149c0f04782b9cf06f5ca59bc246ceea503dbdc
Anita Ruangrotsakun <138700973+anitarua@users.noreply.github.com>
2023-07-27 11:30:26 -0700

    feat: authentication options for the token vending machine (#677)
    * fix: add TokenScopes function example to config file comments
    
    * feat: added basic lambda authorizer example for token vending machine auth option
    
    * added basic amazon cognito user pool example
    
    * addressed pr comments: added AuthenticationMethod enum, better authorizer error messages, separate folder for tvm handler
    
    * fix: corrected path to lambda handler in cdk stack and reinstalled dependencies after moving handler to new directory
    
    * fix: enable CORS on API gateway from the cdk stack
    
    * tvm cdk now creates user pool client and a user

--- examples/nodejs/token-vending-machine/lambda/token-vending-machine/config.ts
+++ examples/nodejs/token-vending-machine/lambda/token-vending-machine/config.ts
@@ -1,10 +1,17 @@
-import {AllDataReadWrite, TopicRole, CacheRole, ExpiresIn, TokenScope, AllTopics, AllCaches} from '@gomomento/sdk';
+import {AllDataReadWrite, TopicRole, CacheRole, ExpiresIn, TokenScope, AllTopics, AllCaches, TokenScopes} from '@gomomento/sdk';
 
 /**
  * Set the scope of permissions for your tokens. 
  * 
  * AllDataReadWrite provides read and write permissions to all of your caches:
  *    export const tokenPermissions: TokenScope =  AllDataReadWrite;
+ * 
+ * TokenScopes provides several functions that will return the permissions you
+ * request for a given cache and topic name.
+ *    export const tokenPermissions: TokenScope = TokenScopes.topicPublishSubscribe("default-cache", AllTopics);
+ * 
+ * You can also set it to subscribe to all caches if you prefer:
+ *    export const tokenPermissions: TokenScope = TokenScopes.topicPublishSubscribe(AllCaches, AllTopics);
  * 
  * You may also provide a bespoke list of permissions for each cache and topic that you have:
  *    export const tokenPermissions: TokenScope =  {
@@ -42,3 +49,17 @@ export const tokenPermissions: TokenScope = {
  * More information here: https://docs.momentohq.com/develop/api-reference/auth-tokens#generateauthtoken-api
  */
 export const tokenExpiresIn: ExpiresIn = ExpiresIn.hours(1);
+
+/**
+ * Set the authentication method for the token vending machine to protect against
+ * unauthorized users. The available options are provided below.
+ * 
+ * Note: when using Amazon Cognito, you'll need to first sign into Cognito to get an ID
+ * token that you'll include in your requests to the Token Vending Machine API. 
+ */
+export enum AuthenticationMethod {
+  Open,             // no authentication
+  LambdaAuthorizer, // use Lambda Authorizer attached to API Gateway
+  AmazonCognito,    // use Cognito user pool authorizer attached to API Gateway
+}
+export const authenticationMethod: AuthenticationMethod = AuthenticationMethod.Open;
