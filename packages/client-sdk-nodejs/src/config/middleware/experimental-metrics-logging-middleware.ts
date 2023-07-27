e72d5cda6788bf03bc11b7318f8055669c25aa48
Chris Price <cprice404@users.noreply.github.com>
2023-07-27 09:42:27 -0700

    feat: add logging implementation of experimental metrics middleware (#675)
    * feat: add logging implementation of experimental metrics middleware
    
    This commit refactors the "Experimental Metrics CSV" middleware to
    be an abstract class that is destination-agnostic. This will allow
    us to provide implementations of it that write to other targets
    besides CSV. Also included in this PR is an implementation that
    writes the metrics to the logger, which is a slightly more scalable
    destination for most of our users to consider enabling.

---
+++ packages/client-sdk-nodejs/src/config/middleware/experimental-metrics-logging-middleware.ts
@@ -0,0 +1,48 @@
+import {MomentoLogger, MomentoLoggerFactory} from '../../';
+import {
+  ExperimentalMetricsMiddleware,
+  ExperimentalMetricsMiddlewareRequestHandler,
+  ExperimentalRequestMetrics,
+} from './impl/experimental-metrics-middleware';
+
+class ExperimentalMetricsLoggingMiddlewareRequestHandler extends ExperimentalMetricsMiddlewareRequestHandler {
+  constructor(parent: ExperimentalMetricsMiddleware, logger: MomentoLogger) {
+    super(parent, logger);
+  }
+
+  emitMetrics(metrics: ExperimentalRequestMetrics): Promise<void> {
+    this.logger.info(JSON.stringify(metrics));
+    return Promise.resolve();
+  }
+}
+
+/**
+ * This middleware enables per-request client-side metrics.  Metrics for each
+ * request will be written to logs; the log data can be analyzed or shared
+ * with Momento to diagnose performance issues.
+ *
+ * The metrics format is currently considered experimental; in a future release,
+ * once the format is considered stable, this class will be renamed to remove
+ * the Experimental prefix.
+ *
+ * WARNING: enabling this middleware may have minor performance implications,
+ * so enable with caution.
+ *
+ * WARNING: depending on your request volume, this middleware will produce a high
+ * volume of log output. If you are writing logs directly to local disk, be aware
+ * of disk usage and make sure you have log rotation / compression enabled via a
+ * tool such as `logrotate`.
+ *
+ * See `advanced.ts` in the examples directory for an example of how to set up
+ * your {Configuration} to enable this middleware.
+ */
+export class ExperimentalMetricsLoggingMiddleware extends ExperimentalMetricsMiddleware {
+  static numActiveRequests = 0;
+
+  constructor(loggerFactory: MomentoLoggerFactory) {
+    super(
+      loggerFactory,
+      (p, l) => new ExperimentalMetricsLoggingMiddlewareRequestHandler(p, l)
+    );
+  }
+}
