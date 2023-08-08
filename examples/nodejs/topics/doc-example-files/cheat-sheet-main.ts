540e41ea2f1bf8eb45bef37e752f84a1bcb4978c
Kirk Kirkconnell <synchrophoto@gmail.com>
2023-08-07 17:43:57 -0700

    Adding topics cheat sheet file.

---
+++ examples/nodejs/topics/doc-example-files/cheat-sheet-main.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {TopicClient, TopicConfigurations, CredentialProvider} from '@gomomento/sdk';
+
+function main() {
+  const cacheClient = new TopicClient({
+    configuration: TopicConfigurations.Default.latest(),
+    credentialProvider: CredentialProvider.fromEnvironmentVariable({
+      environmentVariableName: 'MOMENTO_AUTH_TOKEN',
+    }),
+    defaultTtlSeconds: 60,
+  });
+}
+
+try {
+  main();
+} catch (e) {
+  console.error(`Uncaught exception while running example: ${JSON.stringify(e)}`);
+  throw e;
+}
