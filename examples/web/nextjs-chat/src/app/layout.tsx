6b0fbfde7c6df5f64385903be4495d1fbf35d4ae
pgautier404 <pgautier404@users.noreply.github.com>
2023-08-14 10:40:06 -0700

    fix: fix nextjs chat (#713)
    * fix: fix nextjs-chat example app
    
    * chore: go ahead and verify with CI
    
    * fix: remove obsolete code that was listing caches
    
    * fix: restore auth options type

--- examples/web/nextjs-chat/src/app/layout.tsx
+++ examples/web/nextjs-chat/src/app/layout.tsx
@@ -17,9 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={"h-full"}>
       <body className={inter.className + " h-full"}>
-        <NextAuthProvider>
-          {children}
-        </NextAuthProvider>
+        <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
   );
