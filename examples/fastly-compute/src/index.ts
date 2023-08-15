5a5c9075df9ee054cacb33efbe76a630a1ea28c4
anitarua <anita@momentohq.com>
2023-08-15 09:31:07 -0700

    updated WIP: incorporated Ramya's changes

--- examples/fastly-compute/src/index.ts
+++ examples/fastly-compute/src/index.ts
@@ -1,121 +1,115 @@
-/// <reference types="@fastly/js-compute" />
-import { Backend } from "fastly:backend";
+// Ramya's implementation
 
-// The entry point for your application.
+/// <reference types="@fastly/js-compute" />
+import { env } from "fastly:env";
+import { ConfigStore } from "fastly:config-store";
+
+import { enableDebugLogging } from "fastly:experimental";
+enableDebugLogging(true);
+
+async function get(authToken: string, httpEndpoint: string, backend: string, cacheName: string, key: string): Promise<string> {
+
+  const getUrl = `https://${httpEndpoint}/cache/${cacheName}?key=${key}&token=${authToken}`;
+  const getResp = await fetch(getUrl, { backend: backend });
+  if (getResp.status >= 200 && getResp.status < 300) {
+    const value = await getResp.text();
+    console.log(`successfully retrieved ${key} from cache ${cacheName}: ${value}`);
+    return value;
+  } else if (getResp.status == 404) {
+    const value = await getResp.text();
+    console.log(`${key} not found or cache ${cacheName} not found: ${value}`);
+    return "";
+  } else {
+    throw new Error(`failed to retrieve item: ${getResp.statusText} status: ${getResp.status} cache: ${cacheName}`);
+  }
+}
+
+async function set(authToken: string, httpEndpoint: string, backend: string, cacheName: string, key: string, value: string, ttlSeconds: number = 300) {
+  // set a value in the cache:
+  const setUrl =  `https://${httpEndpoint}/cache/${cacheName}?key=${key}&token=${authToken}&ttl_seconds=${ttlSeconds}`;
+  const setResp = await fetch(
+   setUrl,
+    {
+      method: 'PUT',
+      body: value,
+      backend: backend,
+    }
+  );
+
+  if (setResp.status >= 200 && setResp.status < 300) {
+    console.log(`successfully set ${key} into cache`);
+  } else {
+    throw new Error(
+    `failed to set item: ${setResp.statusText} status: ${setResp.status} cache: ${cacheName}`
+    );
+  }
+}
+
+  // The entry point for your application.
 //
 // Use this fetch event listener to define your main request handling logic. It
 // could be used to route based on the request properties (such as method or
 // path), send the request to a backend, make completely new requests, and/or
 // generate synthetic responses.
-
-
-addEventListener("fetch", (event) => {  
-  event.respondWith(handleRequest(event));
-});
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event: FetchEvent) {
+    // Log service version
+    console.log("FASTLY_SERVICE_VERSION:", env('FASTLY_SERVICE_VERSION') || 'local');
+    // Get the client request.
+    const req = event.request;
+    console.log(`request url: ${req.url}`);
 
-	// Testing basic fetch call
-	
-	const backend = new Backend({
-		name: 'google',
-		target: "google.com",
-		useSSL: true,
-	});
+    const secrets = new ConfigStore("secrets");
+    const authToken = secrets.get("MOMENTO_TOKEN");
+    if (!authToken) {
+      return new Response("Missing required env var MOMENTO_TOKEN", {
+        status: 500,
+      });
+    }
+    const cacheName = secrets.get("MOMENTO_CACHE");
+    if (!cacheName) {
+      return new Response("Missing required env var MOMENTO_CACHE", {
+        status: 500,
+      });
+    }
+    const httpEndpoint = secrets.get("MOMENTO_HTTP_ENDPOINT");
+    if (!httpEndpoint) {
+      return new Response("Missing required env var MOMENTO_HTTP_ENDPOINT", {
+        status: 500,
+      });
+    }
+    const backend = secrets.get("MOMENTO_BACKEND");
+    if (!backend) {
+      return new Response("Missing required env var MOMENTO_BACKEND", {
+        status: 500,
+      });
+    }
 
-	const resp = await fetch("https://www.google.com", {backend: backend.toString()});
-	console.log("google response:", resp.status, resp.statusText);
-	return new Response(await resp.text(), {
-		status: 200,
-		headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
-	});
-
-
-	// const key = "hello";
-	// const value = "hello-momento";
-
-	// Set these values
-	// const cache = "";
-	// const apiKey = "";
-	// const endpoint = "";
-
-	// const backend = new Backend({
-	// 	name: 'momento_service',
-	// 	target: endpoint,
-	// 	useSSL: true,
-	// });
-
-	// const client = new MomentoFetcher(
-	// 	apiKey, 
-	// 	endpoint,
-	// 	backend
-	// );
-	
-	// // setting a value into cache
-	// const setResp = await client.set(cache, key, value);
-	// console.log("setResp", setResp.text);
-
-	// // getting a value from cache
-	// const getResp = await client.get(cache, key);
-	// console.log("getResp", getResp);
-
-	// // deleting a value from cache
-	// const deleteResp = await client.delete(cache, key);
-	// console.log("deleteResp", deleteResp);
-
-	// return new Response(JSON.stringify({ response: getResp }));
-}
-
-class MomentoFetcher {
-	private readonly apiToken: string;
-	private readonly baseurl: string;
-  private readonly backend: Backend;
-  
-	constructor(token: string, endpoint: string, backend: Backend) {
-		this.apiToken = token;
-		this.baseurl = `https://${endpoint}/cache`;
-    this.backend = backend;
-	}
-
-	async get(cacheName: string, key: string) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}`, { backend: this.backend.toString() });
-		if (resp.status < 300) {
-			console.log(`successfully retrieved ${key} from cache`)
-		} else {
-			throw new Error(`failed to retrieve item from cache: ${cacheName} with status code ${resp.status}`);
-		}
-		
-		return await resp.text();
-	}
-
-	async set(cacheName: string, key: string, value: string, ttl_seconds: number = 30) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}&ttl_seconds=${ttl_seconds}`, {
-			method: 'PUT',
-			body: value,
-      backend: this.backend.toString()
-		});
-
-		if (resp.status < 300) {
-			console.log(`successfully set ${key} into cache`);
-		} else {
-			throw new Error(`failed to set item into cache message: ${resp.statusText} status: ${resp.status} cache: ${cacheName}`);
-		}
-
-		return resp;
-	}
-
-	async delete(cacheName: string, key: string) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}`, {
-			method: 'DELETE',
-      backend: this.backend.toString()
-		});
-		if (resp.status < 300) {
-			console.log(`successfully deleted ${key} from cache`);
-		} else {
-			throw new Error(`failed to delete ${key} from cache. Message: ${resp.statusText}; Status: ${resp.status} cache: ${cacheName}`);
-		}
-
-		return resp.text();
-	}
-}
+    try {
+      console.log("Running momento example");
+      const testKey = "IAmATestKey";
+      const testValue = "Far out in the uncharted backwaters of the unfashionable end of the western spiral arm of the Galaxy lies a small unregarded yellow sun";
+      const getResp = await get(authToken, httpEndpoint, backend, cacheName, testKey);
+      console.log(`${getResp}`);
+      const setResp = await set(authToken, httpEndpoint, backend, cacheName, testKey, testValue, 10);
+      const getRespAfterSet = await get(authToken, httpEndpoint, backend, cacheName, testKey);
+      console.log(`${getRespAfterSet}`);
+      // Send a default synthetic response.
+      return new Response("Tested get and set successfully", {
+        status: 200,
+        headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
+      });
+  } catch (e) {
+    if (e instanceof Error) {
+      return new Response(`Caught exception: ${e.message}`, {
+        status: 500,
+      });
+    } else {
+      return new Response("Unknown error", {
+        status: 500,
+      });
+    }
+  }
+};
 
