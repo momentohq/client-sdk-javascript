eab8eac905448e54cb8c3bba463b7fb24a56fb6e
anitarua <anita@momentohq.com>
2023-08-16 14:36:29 -0700

    chore: update fastly readme to explain secret store vs config store usage

--- examples/fastly-compute/src/index.ts
+++ examples/fastly-compute/src/index.ts
@@ -34,6 +34,7 @@ async function handleRequest(event: FetchEvent) {
   console.log('Connected to the Fastly Config Store');
 
   // Get all required information from the Config Store
+  // Note: for production environments, the Momento auth token should be saved in a Fastly Secret Store
   const authToken = secrets.get('MOMENTO_TOKEN');
   if (!authToken) {
     return new Response('Missing required env var MOMENTO_TOKEN', {
