e6d4e4a79a25723bf4926d986eb0750092c55b43
Pete Gautier <pete@momentohq.com>
2023-08-24 13:47:56 -0700

    fix: fix incorrect log message

--- examples/web/vector-index/doc-example-files/doc-examples-web-apis.ts
+++ examples/web/vector-index/doc-example-files/doc-examples-web-apis.ts
@@ -27,7 +27,7 @@ async function example_API_ListIndexes(vectorClient: PreviewVectorClient) {
 async function example_API_DeleteIndex(vectorClient: PreviewVectorClient) {
   const result = await vectorClient.deleteIndex("test-index")
   if (result instanceof DeleteIndex.Success) {
-    console.log("Index 'test-index' created");
+    console.log("Index 'test-index' deleted");
   } else if (result instanceof DeleteIndex.Error) {
     throw new Error(
       `An error occurred while attempting to delete index 'test-index': ${result.errorCode()}: ${result.toString()}`
