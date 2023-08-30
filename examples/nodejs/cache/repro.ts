13fdb512cd0737666f82ad8a1f87935be9b9da6f
Pratik Agarwal <pratik@momentohq.com>
2023-08-30 12:32:55 -0300

    chore: adding simple repro

---
+++ examples/nodejs/cache/repro.ts
@@ -0,0 +1,34 @@
+import {CacheGet, CreateCache, CacheSet, CacheClient, Configurations, CredentialProvider} from '@gomomento/sdk';
+
+async function main() {
+  const clients: CacheClient[] = [];
+
+  for (let i = 0; i < 2; i++) {
+    const momento = await CacheClient.create({
+      configuration: Configurations.Laptop.v1(),
+      credentialProvider: CredentialProvider.fromEnvironmentVariable({
+        environmentVariableName: 'MOMENTO_AUTH_TOKEN',
+      }),
+      defaultTtlSeconds: 60,
+    });
+    clients.push(momento);
+  }
+
+  for (let i = 0; i < 1000; i++) {
+    const client = clients[i % 2];
+    const createCacheResponse = await client.createCache('cache');
+    await client.listCaches();
+    if (createCacheResponse instanceof CreateCache.Success) {
+      await client.deleteCache('cache');
+    }
+  }
+}
+
+main()
+  .then(() => {
+    console.log('success!!');
+  })
+  .catch((e: Error) => {
+    console.error(`Uncaught exception while running example: ${e.message}`);
+    throw e;
+  });
