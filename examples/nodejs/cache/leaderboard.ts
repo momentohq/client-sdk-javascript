8a280f7f9f6fd6cf05ab9a33095b1bd0575d2295
anitarua <anita@momentohq.com>
2023-09-25 17:02:01 -0700

    added instanceof checks to example

--- examples/nodejs/cache/leaderboard.ts
+++ examples/nodejs/cache/leaderboard.ts
@@ -1,4 +1,12 @@
 import {LeaderboardClient, LeaderboardConfigurations, CredentialProvider, SortedSetOrder} from '@gomomento/sdk';
+import {
+  LeaderboardDelete,
+  LeaderboardFetch,
+  LeaderboardGetRank,
+  LeaderboardLength,
+  LeaderboardRemoveElements,
+  LeaderboardUpsert,
+} from '@gomomento/sdk-core';
 
 async function main() {
   const client = new LeaderboardClient({
@@ -14,68 +22,70 @@ async function main() {
     [456n, 200],
     [789n, 300],
   ]);
-  const upsertResp = await client.leaderboardUpsert(
-    "my-cache", 
-    "my-leaderboard", 
-    elements
-  );
-  console.log("Upsert elements:", upsertResp.toString());
-  
+  const upsertResp = await client.leaderboardUpsert('my-cache', 'my-leaderboard', elements);
+  if (upsertResp instanceof LeaderboardUpsert.Success) {
+    console.log('Upsert success');
+  } else if (upsertResp instanceof LeaderboardUpsert.Error) {
+    console.log('Upsert error:', upsertResp.message());
+  }
+
   // fetch by score
-  const fetchByScore = await client.leaderboardFetchByScore(
-    "my-cache", 
-    "my-leaderboard", 
-    {
-      minScore: 100,
-      maxScore: 500,
-      order: SortedSetOrder.Descending,
-      offset: 10,
-      count: 100,
-    }
-  );
-  console.log("Fetch by score:", fetchByScore.toString());
+  const fetchByScore = await client.leaderboardFetchByScore('my-cache', 'my-leaderboard', {
+    minScore: 100,
+    maxScore: 500,
+    order: SortedSetOrder.Descending,
+    offset: 10,
+    count: 100,
+  });
+  if (fetchByScore instanceof LeaderboardFetch.Success) {
+    console.log('Fetch by score success');
+  } else if (fetchByScore instanceof LeaderboardFetch.Error) {
+    console.log('Fetch by score error:', fetchByScore.message());
+  }
 
   // fetch by rank
-  const fetchByRank = await client.leaderboardFetchByRank(
-    "my-cache", 
-    "my-leaderboard", 
-    {
-      startRank: 1,
-      endRank: 5,
-      order: SortedSetOrder.Ascending
-    }
-  );
-  console.log("Fetch by rank:", fetchByRank.toString());
+  const fetchByRank = await client.leaderboardFetchByRank('my-cache', 'my-leaderboard', {
+    startRank: 1,
+    endRank: 5,
+    order: SortedSetOrder.Ascending,
+  });
+  if (fetchByRank instanceof LeaderboardFetch.Success) {
+    console.log('Fetch by rank success');
+  } else if (fetchByRank instanceof LeaderboardFetch.Error) {
+    console.log('Fetch by rank error:', fetchByRank.message());
+  }
 
   // get rank
-  const getRank = await client.leaderboardGetRank(
-    "my-cache", 
-    "my-leaderboard", 
-    123n
-  );
-  console.log("Get rank:", getRank.toString());
+  const getRank = await client.leaderboardGetRank('my-cache', 'my-leaderboard', 123n);
+  if (getRank instanceof LeaderboardGetRank.Success) {
+    console.log('Get rank success');
+  } else if (getRank instanceof LeaderboardGetRank.Error) {
+    console.log('Get rank error:', getRank.message());
+  }
 
   // length
-  const lengthResp = await client.leaderboardLength(
-    "my-cache", 
-    "my-leaderboard", 
-  );
-  console.log("Get leaderboard length:", lengthResp.toString());
+  const lengthResp = await client.leaderboardLength('my-cache', 'my-leaderboard');
+  if (lengthResp instanceof LeaderboardLength.Success) {
+    console.log('Get leaderboard length success');
+  } else if (lengthResp instanceof LeaderboardLength.Error) {
+    console.log('Get leaderboard length error:', lengthResp.message());
+  }
 
   // remove
-  const removeResp = await client.leaderboardRemoveElements(
-    "my-cache", 
-    "my-leaderboard", 
-    [123n, 456n, 789n]
-  );
-  console.log("Remove elements:", removeResp.toString());
+  const removeResp = await client.leaderboardRemoveElements('my-cache', 'my-leaderboard', [123n, 456n, 789n]);
+  if (removeResp instanceof LeaderboardRemoveElements.Success) {
+    console.log('Remove elements success');
+  } else if (removeResp instanceof LeaderboardRemoveElements.Error) {
+    console.log('Remove elements error:', removeResp.message());
+  }
 
   // delete
-  const deleteResp = await client.leaderboardDelete(
-    "my-cache", 
-    "my-leaderboard", 
-  );
-  console.log("Delete leaderboard:", deleteResp.toString());
+  const deleteResp = await client.leaderboardDelete('my-cache', 'my-leaderboard');
+  if (deleteResp instanceof LeaderboardDelete.Success) {
+    console.log('Delete leaderboard success');
+  } else if (deleteResp instanceof LeaderboardDelete.Error) {
+    console.log('Delete leaderboard error:', deleteResp.message());
+  }
 }
 
 main()
