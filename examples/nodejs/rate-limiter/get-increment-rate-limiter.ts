d1fd63e260ec8e01e9b9eef3f0ace65919b2830b
Pratik Agarwal <pratik@momentohq.com>
2023-10-31 17:36:31 -0300

    chore: make latency cal bug free

--- examples/nodejs/rate-limiter/get-increment-rate-limiter.ts
+++ examples/nodejs/rate-limiter/get-increment-rate-limiter.ts
@@ -12,29 +12,35 @@ export class GetIncrementRateLimiter extends AbstractRateLimiter {
 
   public async acquire(id: string): Promise<boolean> {
     const start = Date.now();
+
+    let allowed = false;
+
     const currentMinuteKey = this.generateMinuteKey(id);
     // we do not pass a TTL to this; we don't know if the key for this user was present or not
     const getResp = await this._client.get('rate-limiter', currentMinuteKey);
-    const latency = Date.now() - start;
 
     if (getResp instanceof CacheGet.Hit) {
       if (parseInt(getResp.value(), 10) < this._limit) {
         await this._client.increment('rate-limiter', currentMinuteKey);
-        this.metrics.recordSuccess(latency);
-        return true;
+        allowed = true;
       }
-      this.metrics.recordThrottle(latency);
-      return false;
     } else if (getResp instanceof CacheGet.Miss) {
       // first call to key so we set TTL now to 60 seconds
       await this._client.increment('rate-limiter', currentMinuteKey, 1, {ttl: 60000});
-      this.metrics.recordSuccess(latency);
+      allowed = true;
       return true;
     } else if (getResp instanceof CacheGet.Error) {
       console.error(`Error while getting value for key ${currentMinuteKey}` + getResp.message());
       this.metrics.recordErrors();
     }
 
-    throw new Error(`unexpected rate limiter state`);
+    const latency = Date.now() - start;
+    if (allowed) {
+      this.metrics.recordSuccess(latency);
+    } else {
+      this.metrics.recordThrottle(latency);
+    }
+
+    return allowed;
   }
 }
--- examples/nodejs/rate-limiter/increment-only-rate-limiter.ts
+++ examples/nodejs/rate-limiter/increment-only-rate-limiter.ts
@@ -13,29 +13,31 @@ export class IncrementRateLimiter extends AbstractRateLimiter {
   public async acquire(id: string): Promise<boolean> {
     const start = Date.now();
 
+    let allowed = false;
+
     const currentMinuteKey = this.generateMinuteKey(id);
-    const requestStart = Date.now();
     // we do not pass a TTL to this; we don't know if the key for this user was present or not
     const resp = await this._client.increment('rate-limiter', currentMinuteKey);
-    const requestLatency = Date.now() - requestStart;
-    const latency = Date.now() - start;
-    console.log(`Individual latency is ${requestLatency}`);
+
     if (resp instanceof CacheIncrement.Success) {
       if (resp.value() <= this._limit) {
         if (resp.value() === 1) {
           await this._client.updateTtl('rate-limiter', id, 60000);
         }
-        this.metrics.recordSuccess(latency);
-        return true;
+        allowed = true;
       }
-      this.metrics.recordThrottle(latency);
-      return false;
     } else if (resp instanceof CacheIncrement.Error) {
       console.error('Error while incrementing ' + resp.message());
       this.metrics.recordErrors();
-      return false;
     }
 
-    throw new Error(`unexpected rate limiter state`);
+    const latency = Date.now() - start;
+    if (allowed) {
+      this.metrics.recordSuccess(latency);
+    } else {
+      this.metrics.recordThrottle(latency);
+    }
+
+    return allowed;
   }
 }
