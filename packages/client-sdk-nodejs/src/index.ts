ec27f26752f5c631245a9490475f7f8896ddfdaa
anitarua <anita@momentohq.com>
2023-10-31 18:15:47 -0700

    remove repeated export

--- packages/client-sdk-nodejs/src/index.ts
+++ packages/client-sdk-nodejs/src/index.ts
@@ -202,7 +202,6 @@ export {ExperimentalRequestLoggingMiddleware} from './config/middleware/experime
 export {ExperimentalMetricsCsvMiddleware} from './config/middleware/experimental-metrics-csv-middleware';
 export {ExperimentalMetricsLoggingMiddleware} from './config/middleware/experimental-metrics-logging-middleware';
 export {ExampleAsyncMiddleware} from './config/middleware/example-async-middleware';
-export {ExperimentalMetricsLoggingMiddleware} from './config/middleware/experimental-metrics-logging-middleware';
 
 export {
   ICacheClient,
