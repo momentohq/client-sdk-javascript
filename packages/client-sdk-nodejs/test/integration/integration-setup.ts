087a2386cfb7210e7e93b67d053efc345bb53891
Anita Ruangrotsakun <138700973+anitarua@users.noreply.github.com>
2023-11-02 14:18:03 -0700

    fix: lint error (#1000)

--- packages/client-sdk-nodejs/test/integration/integration-setup.ts
+++ packages/client-sdk-nodejs/test/integration/integration-setup.ts
@@ -88,7 +88,8 @@ function momentoClientForTesting(): CacheClient {
 
 function momentoClientForTestingWithSessionToken(): CacheClient {
   return new CacheClient({
-    configuration: Configurations.Laptop.latest().withClientTimeoutMillis(60000),
+    configuration:
+      Configurations.Laptop.latest().withClientTimeoutMillis(60000),
     credentialProvider: sessionCredsProvider(),
     defaultTtlSeconds: 1111,
   });
