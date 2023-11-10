013a29645cf711f0e31c755fc8f47c645a74396d
Michael Landis <michael@momentohq.com>
2023-11-10 10:34:23 -0800

    chore: update generated types and consume searchhit score change (#1026)
    Updates generated types in Web SDK and Node JS packages and consumes the name change in SearchHit from distance to score.

--- packages/client-sdk-nodejs/src/internal/vector-index-data-client.ts
+++ packages/client-sdk-nodejs/src/internal/vector-index-data-client.ts
@@ -273,7 +273,7 @@ export class VectorIndexDataClient implements IVectorIndexDataClient {
               new VectorSearch.Success(
                 resp.hits.map(hit => ({
                   id: hit.id,
-                  score: hit.distance,
+                  score: hit.score,
                   metadata: hit.metadata.reduce((acc, metadata) => {
                     const field = metadata.field;
                     switch (metadata.value) {
