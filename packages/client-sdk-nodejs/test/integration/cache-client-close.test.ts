fb48788fdf0d4fe362d4cf7b34151fbd1e393709
Pratik Agarwal <pratik@momentohq.com>
2024-03-06 15:21:57 -0800

    chore: address Chris's comments

--- packages/client-sdk-nodejs/test/integration/cache-client-close.test.ts
+++ packages/client-sdk-nodejs/test/integration/cache-client-close.test.ts
@@ -1,23 +1,18 @@
 import {expectWithMessage} from '@gomomento/common-integration-tests';
 
-import {GetBatch, ICacheClient} from '@gomomento/sdk-core';
+import {GetBatch} from '@gomomento/sdk-core';
 import {SetupIntegrationTestWithMiddleware} from './integration-setup';
 
 const {cacheClient, cacheName} = SetupIntegrationTestWithMiddleware();
 
-runTestWithClientCloseToMakeSureTestDoesntRunForever(cacheClient);
-export function runTestWithClientCloseToMakeSureTestDoesntRunForever(
-  cacheClient: ICacheClient
-) {
-  describe('dummy test to close client', () => {
-    it('getBatch happy path with all misses', async () => {
-      const keys = ['a', 'b', 'c', '1', '2', '3'];
-      const response = await cacheClient.getBatch(cacheName, keys);
+describe("Test exercises closing a client and jest doesn't hang", () => {
+  it('getBatch happy path with all misses', async () => {
+    const keys = ['a', 'b', 'c', '1', '2', '3'];
+    const response = await cacheClient.getBatch(cacheName, keys);
 
-      // Check get batch response
-      expectWithMessage(() => {
-        expect(response).toBeInstanceOf(GetBatch.Success);
-      }, `expected SUCCESS for keys ${keys.toString()}, received ${response.toString()}`);
-    });
+    // Check get batch response
+    expectWithMessage(() => {
+      expect(response).toBeInstanceOf(GetBatch.Success);
+    }, `expected SUCCESS for keys ${keys.toString()}, received ${response.toString()}`);
   });
-}
+});
