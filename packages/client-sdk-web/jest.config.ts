20cf19035e3d0c074d9308d14df9986e5023d9d1
anitarua <anita@momentohq.com>
2024-03-14 17:07:35 -0700

    put jest ci spec reporter back in web sdk jest config

--- packages/client-sdk-web/jest.config.ts
+++ packages/client-sdk-web/jest.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
     // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
     "uuid": require.resolve('uuid'),
   },
-  // reporters: ["jest-ci-spec-reporter"]
+  reporters: ["jest-ci-spec-reporter"]
 };
 
 export default config;
