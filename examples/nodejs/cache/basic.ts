d383cdd4128a479b9a39345cec1d499a82391d0c
Matt Straathof <matt.straat@gmail.com>
2024-03-18 12:14:59 -0700

    chore: working pmap

--- examples/nodejs/cache/basic.ts
+++ examples/nodejs/cache/basic.ts
@@ -1,8 +1,7 @@
 import {CacheGet, CreateCache, CacheSet, CacheClient, Configurations, CredentialProvider} from '@gomomento/sdk';
-import pMap from 'p-map';
+import * as pmap from 'p-map';
 
 async function main() {
-
   const momento = await CacheClient.create({
     configuration: Configurations.Laptop.v1(),
     credentialProvider: CredentialProvider.fromEnvironmentVariable({
@@ -11,13 +10,9 @@ async function main() {
     defaultTtlSeconds: 60,
   });
 
-
-  const actions = [
-    () => momento.set('cache', 'foo', 'FOO'),
-    () => momento.get('cache', 'foo')
-  ];
+  const actions = [() => momento.set('cache', 'foo', 'FOO'), () => momento.get('cache', 'foo')];
   const mapper = (action: () => Promise<any>): Promise<any> => action();
-  console.log(await pMap(actions, mapper,{concurrency: 1}));
+  console.log(await pmap.default(actions, mapper, {concurrency: 1}));
 
   console.log('Storing key=foo, value=FOO');
   const setResponse = await momento.set('cache', 'foo', 'FOO');
