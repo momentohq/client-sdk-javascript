b6727b52cc304b413c9e34772d13c8f5a649c368
Pratik Agarwal <pratik@momentohq.com>
2024-03-26 11:52:29 -0300

    add basic redis

--- examples/nodejs/cache/basic.ts
+++ examples/nodejs/cache/basic.ts
@@ -30,17 +30,18 @@ async function main() {
 
     const promise = momento.set('cache', `foo${i}`, 'FOO')
       .then(response => {
-        const endTime = process.hrtime.bigint(); // End timing
-        const latency = Number((endTime - startTime) / BigInt(1000000)); // Convert to milliseconds
-        histogram.recordValue(latency); // Record latency
-
         if (response instanceof CacheSet.Error) {
           console.log(`Request ${i}: Error`, response);
         }
       })
       .catch(error => {
         console.log(`Request ${i}: Failed with exception`, error);
-        throw error;
+        // Don't throw error here to ensure latency recording
+      })
+      .finally(() => {
+        const endTime = process.hrtime.bigint(); // End timing
+        const latency = Number((endTime - startTime) / BigInt(1000000)); // Convert to milliseconds
+        histogram.recordValue(latency); // Record latency regardless of promise outcome
       });
 
     promises.push(promise);
@@ -49,15 +50,13 @@ async function main() {
   await Promise.all(promises)
     .then(() => {
       console.log('All promises have been processed.');
-
-      // Log computed statistics
+    })
+    .finally(() => {
+      // Log computed statistics after all promises, including failed ones, have been processed.
       console.log(`Latency p50: ${histogram.getValueAtPercentile(50)} ms`);
       console.log(`Latency p90: ${histogram.getValueAtPercentile(90)} ms`);
       console.log(`Latency p99: ${histogram.getValueAtPercentile(99)} ms`);
       console.log(`Latency p100: ${histogram.getValueAtPercentile(100)} ms`);
-    })
-    .catch(error => {
-      console.log('One or more promises failed.', error);
     });
 
   momento.close();
