70a47f5401372782b7739dbba90df7db0f56fc67
Pratik Agarwal <pratik@momentohq.com>
2024-03-26 11:11:04 -0300

    change endpoint

--- examples/nodejs/cache/basic-redis.ts
+++ examples/nodejs/cache/basic-redis.ts
@@ -1,39 +1,47 @@
 import Redis from "ioredis";
 
 async function main() {
-
   const redisClient = new Redis({
-    host: 'raider-repro.tlghey.ng.0001.usw2.cache.amazonaws.com:6379',
+    host: 'raider-repro.tlghey.ng.0001.usw2.cache.amazonaws.com',
     port: 6379,
     connectTimeout: 17000,
     maxRetriesPerRequest: 4,
     retryStrategy: (times) => Math.min(times * 30, 1000),
-    reconnectOnError: (error)  => {
+    reconnectOnError: (error) => {
       const targetErrors = [/READONLY/, /ETIMEDOUT/];
       return targetErrors.some((targetError) => targetError.test(error.message));
     }
   });
 
   redisClient.on('error', function (error) {
-    console.error(error);
+    console.error('Redis client error:', error);
   });
 
-  const promises = [];
+  let promises = [];
   for (let i = 0; i < 10000; i++) {
-    promises.push(redisClient.set('foo'.concat(String(i)), 'FOO'));
-  }
-  await Promise.all(promises);
+    // Wrap each `redisClient.set` operation to individually handle its resolution and rejection.
+    const promise = redisClient.set('foo'.concat(String(i)), 'FOO')
+      .then(result => {
+        // Log success status or value, if necessary.
+        // console.log(`Success for key foo${i}:`, result);
+      })
+      .catch(error => {
+        // Log the error status here.
+        console.error(`Error for key foo${i}:`, error);
+      });
 
-  // Wait for all promises to resolve or reject.
+    promises.push(promise);
+  }
+
+  // Use Promise.allSettled to wait for all promises to either resolve or reject.
+  // This method allows handling of each promise's outcome individually.
   await Promise.all(promises)
-    .then(() => {
-      console.log('All promises have been processed.');
-    })
-    .catch(error => {
-      // This will catch any rejections that were re-thrown in the catch blocks above.
-      console.log('One or more promises failed.', error);
+    .then((results) => {
+      // Optionally, process the results array if you need to perform any aggregated analysis.
+      console.log('All operations have been processed.');
     });
 }
+
 
 main()
   .then(() => {
