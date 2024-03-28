28776c39676ad2c437b5ad40fc844cb885f42f41
rishtigupta <rishti@momentohq.com>
2024-03-28 16:38:38 -0700

    fix: correct filename

--- examples/nodejs/get-set-batch-perf-test/utils/perf-test-utils.ts
+++ examples/nodejs/get-set-batch-perf-test/utils/perf-test-utils.ts
@@ -82,9 +82,7 @@ function writeStatsToCSV(
   histogram: hdr.Histogram,
   totalItemSizeBytes: number
 ): void {
-  const currentDate = new Date();
-  const dateTimeString = currentDate.toLocaleString().replace(/[/,\s:]/g, '_');
-  const filename = `perf_test_stats_${dateTimeString}.csv`;
+  const filename = 'perf_test_stats.csv';
   const header = 'requestType,BatchSize,itemSize,TotalCount,Min,p50,p90,p99,p99.9,Max,TotalItemSizeBytes\n';
   const stats =
     `${requestType},${batchSize},${itemSize},${histogram.totalCount},${histogram.minNonZeroValue},` +
