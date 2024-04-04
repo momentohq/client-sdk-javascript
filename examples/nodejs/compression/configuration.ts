2129a904f2706554d8c3b729e9907b248932180e
rishtigupta <rishti@momentohq.com>
2024-04-04 14:52:53 -0700

    fix: remove example code for now

--- examples/nodejs/compression/configuration.ts
+++
@@ -1,4 +0,0 @@
-configuration.withCompression({
-  compressionExtensions: CompressionExtensions.load(),
-  automaticDecompression: AutomaticDecompression.Enabled,
-})
--- examples/nodejs/compression/set.ts
+++
@@ -1,8 +0,0 @@
-const setResponse = await cacheClient.set(
-  'my-cache',
-  'my-key',
-  'my-value',
-  {
-    compression: CompressionMode.Default,
-  }
-);
