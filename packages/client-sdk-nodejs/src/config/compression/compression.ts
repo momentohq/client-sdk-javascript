6e39dfe53fa87ecf4ae4fc2cb22ade531cc699b6
Chris Price <chris@momentohq.com>
2024-04-03 21:47:42 -0700

    chore: add docstrings for new compression options

--- packages/client-sdk-nodejs/src/config/compression/compression.ts
+++ packages/client-sdk-nodejs/src/config/compression/compression.ts
@@ -9,12 +9,34 @@ export interface Compression {
   decompressIfCompressed(value: Uint8Array): Promise<Uint8Array>;
 }
 
+/**
+ * Configuration options for compression capabilities.
+ */
 export interface CompressionProps {
+  /**
+   * This property is used to register the compression extensions from the add-on library. To use this feature,
+   * you will need to install the compression extensions library `@gomomento/sdk-nodejs-compression`, and then
+   * import and call `CompressionExtensions.load()`.
+   */
   compressionExtensions: Compression;
+  /**
+   * This property controls whether calls to `CacheClient.get` should automatically decompress the returned values.
+   */
   automaticDecompression: AutomaticDecompression;
 }
 
+/**
+ * The mode of automatic decompression for calls to `CacheClient.get`.
+ */
 export enum AutomaticDecompression {
+  /**
+   * Automatic decompression is enabled; if a value retrieved via `CacheClient.get` is compressed, it will be automatically decompressed.
+   * @type {AutomaticDecompression.Enabled}
+   */
   Enabled = 'Enabled',
+  /**
+   * Automatic decompression is disabled; if a value retrieved via `CacheClient.get` is compressed, it will be returned as-is.q
+   * @type {AutomaticDecompression.Disabled}
+   */
   Disabled = 'Disabled',
 }
--- packages/core/src/compression/compression-mode.ts
+++ packages/core/src/compression/compression-mode.ts
@@ -1,5 +1,20 @@
+/**
+ * When compressing a value, you may choose between these different modes to control the trade-off between compression speed and compression ratio.
+ */
 export enum CompressionMode {
+  /**
+   * The default compression mode; provides a good balance between speed and compression ratio.
+   * @type {CompressionMode.Default}
+   */
   Default = 'Default',
+  /**
+   * The fastest compression mode; provides the fastest compression speed, prioritizing reduction in CPU usage over compression ratio.
+   * @type {CompressionMode.Fast}
+   */
   Fast = 'Fast',
+  /**
+   * The smallest compression mode; provides the best compression ratio, prioritizing reduction in storage space over compression speed.
+   * @type {CompressionMode.Smallest}
+   */
   Smallest = 'Smallest',
 }
