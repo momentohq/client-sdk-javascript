ac6b175f0613fc85f88c1cbd89814f8a49bcf051
Chris Price <chris@momentohq.com>
2024-05-09 16:08:46 -0700

    chore: working on example readme templates

--- examples/nodejs/aws/lambda-examples/cloudwatch-metrics/infrastructure/lib/momento-metrics-stack.ts
+++ examples/nodejs/aws/lambda-examples/cloudwatch-metrics/infrastructure/lib/momento-metrics-stack.ts
@@ -7,18 +7,11 @@ import * as secrets from 'aws-cdk-lib/aws-secretsmanager';
 import {FilterPattern, LogGroup, RetentionDays} from 'aws-cdk-lib/aws-logs';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as iam from 'aws-cdk-lib/aws-iam';
-import {
-  Dashboard,
-  GraphWidget,
-  MathExpression,
-  Metric,
-  Stats,
-  Unit,
-} from 'aws-cdk-lib/aws-cloudwatch';
-import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+import {Dashboard, GraphWidget, MathExpression, Metric, Stats, Unit} from 'aws-cdk-lib/aws-cloudwatch';
+import {DockerImageAsset} from 'aws-cdk-lib/aws-ecr-assets';
 import 'dotenv/config';
 
-const validStackConfigs = ["lambda", "ecs", "dashboard-only"];
+const validStackConfigs = ['lambda', 'ecs', 'dashboard-only'];
 
 export class MomentoMetricsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -50,9 +43,9 @@ export class MomentoMetricsStack extends cdk.Stack {
     Step 3: determine which log group name to use when creating our log group.
     */
     const configToLogGroupName = new Map([
-      ["lambda", '/aws/lambda/MomentoMetricsMiddlewareLambda'],
-      ["ecs", '/aws/ecs/MomentoMetricsMiddlewareECS'],
-      ["dashboard-only", dashboardOnlyLogGroupName]
+      ['lambda', '/aws/lambda/MomentoMetricsMiddlewareLambda'],
+      ['ecs', '/aws/ecs/MomentoMetricsMiddlewareECS'],
+      ['dashboard-only', dashboardOnlyLogGroupName],
     ]);
     const logGroupName = configToLogGroupName.get(stackConfig);
 
@@ -64,15 +57,15 @@ export class MomentoMetricsStack extends cdk.Stack {
     // Step 4: create the Lambda or ECS example as specified. Otherwise skip this step
     // and create only the dashboard.
     switch (stackConfig) {
-      case "lambda": {
+      case 'lambda': {
         this.setUpLambdaFunction(apiKeySecret);
         break;
       }
-      case "ecs": {
+      case 'ecs': {
         this.setUpEcsCluster(logGroup, apiKeySecret);
         break;
       }
-      case "dashboard-only": {
+      case 'dashboard-only': {
         console.log('Skipping to dashboard creation');
         break;
       }
@@ -83,7 +76,7 @@ export class MomentoMetricsStack extends cdk.Stack {
 
     /* Step 5:
     The Momento experimental metrics middleware produces JSON logs with metrics about each Momento request. An example log entry looks like this:
-        (Momento: _ExperimentalMetricsLoggingMiddleware): 
+        (Momento: _ExperimentalMetricsLoggingMiddleware):
         {
           "momento": {
             "numActiveRequestsAtStart": 1,
@@ -99,11 +92,11 @@ export class MomentoMetricsStack extends cdk.Stack {
             "connectionID": "0"
           }
         }
-    
+
     The `createMetricFilters` function creates CloudWatch Metric Filters to extract metrics from these log messages. The metric filters are attached to the log group that we created above.
     */
     this.createMetricFilters(logGroup);
-    
+
     // Step 6: create the CloudWatch dashboard and add all the widgets (graphs)
     const dashboard = new Dashboard(this, 'MomentoMetricsCDKExampleDashboard', {
       dashboardName: 'MomentoMetricsCDKExampleDashboard',
@@ -132,8 +125,8 @@ export class MomentoMetricsStack extends cdk.Stack {
   setUpEcsCluster(logGroup: cdk.aws_logs.LogGroup, apiKeySecret: cdk.aws_secretsmanager.Secret) {
     const cluster = new ecs.Cluster(this, 'MomentoMetricsExampleFargateCluster');
 
-    const imageAsset = new DockerImageAsset(this, "MomentoMetricsECSDockerImage", {
-      directory: path.join(__dirname, "../../docker")
+    const imageAsset = new DockerImageAsset(this, 'MomentoMetricsECSDockerImage', {
+      directory: path.join(__dirname, '../../docker'),
     });
 
     const taskDefinition = new ecs.FargateTaskDefinition(this, 'MomentoMetricsMiddlewareECS');
@@ -141,23 +134,18 @@ export class MomentoMetricsStack extends cdk.Stack {
       image: ecs.ContainerImage.fromDockerImageAsset(imageAsset),
       logging: new ecs.AwsLogDriver({
         logGroup: logGroup,
-        streamPrefix: "MomentoMetricsMiddlewareECS"
-      })
+        streamPrefix: 'MomentoMetricsMiddlewareECS',
+      }),
     });
 
     const policy = new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
-      actions: [
-        "secretsmanager:GetSecretValue",
-        "logs:PutLogEvents",
-        "logs:CreateLogStream",
-        "ecs:*"
-      ],
+      actions: ['secretsmanager:GetSecretValue', 'logs:PutLogEvents', 'logs:CreateLogStream', 'ecs:*'],
     });
     policy.addResources(
-      apiKeySecret.secretArn, 
-      logGroup.logGroupArn, 
-      cluster.clusterArn, 
+      apiKeySecret.secretArn,
+      logGroup.logGroupArn,
+      cluster.clusterArn,
       taskDefinition.taskDefinitionArn
     );
     taskDefinition.addToTaskRolePolicy(policy);
@@ -185,8 +173,8 @@ export class MomentoMetricsStack extends cdk.Stack {
       metricValue: '$.momento.duration',
       unit: Unit.MILLISECONDS,
       dimensions: {
-        'status': '$.momento.status',
-      }
+        status: '$.momento.status',
+      },
     });
 
     logGroup.addMetricFilter('MetricFilterDurationAndRequestType', {
@@ -196,8 +184,8 @@ export class MomentoMetricsStack extends cdk.Stack {
       metricValue: '$.momento.duration',
       unit: Unit.MILLISECONDS,
       dimensions: {
-        'requestType': '$.momento.requestType',
-      }
+        requestType: '$.momento.requestType',
+      },
     });
 
     logGroup.addMetricFilter('MetricFilterRequestSize', {
@@ -215,8 +203,8 @@ export class MomentoMetricsStack extends cdk.Stack {
       metricValue: '$.momento.requestSize',
       unit: Unit.BYTES,
       dimensions: {
-        'status': '$.momento.status',
-      }
+        status: '$.momento.status',
+      },
     });
 
     logGroup.addMetricFilter('MetricFilterRequestSizeAndRequestType', {
@@ -226,8 +214,8 @@ export class MomentoMetricsStack extends cdk.Stack {
       metricValue: '$.momento.requestSize',
       unit: Unit.BYTES,
       dimensions: {
-        'requestType': '$.momento.requestType',
-      }
+        requestType: '$.momento.requestType',
+      },
     });
 
     logGroup.addMetricFilter('MetricFilterResponseSize', {
@@ -245,8 +233,8 @@ export class MomentoMetricsStack extends cdk.Stack {
       metricValue: '$.momento.responseSize',
       unit: Unit.BYTES,
       dimensions: {
-        'status': '$.momento.status',
-      }
+        status: '$.momento.status',
+      },
     });
 
     logGroup.addMetricFilter('MetricFilterResponseSizeAndRequestType', {
@@ -256,8 +244,8 @@ export class MomentoMetricsStack extends cdk.Stack {
       metricValue: '$.momento.responseSize',
       unit: Unit.BYTES,
       dimensions: {
-        'requestType': '$.momento.requestType',
-      }
+        requestType: '$.momento.requestType',
+      },
     });
   }
 
@@ -271,19 +259,19 @@ export class MomentoMetricsStack extends cdk.Stack {
           metricName: 'Latency',
           namespace: 'MomentoMetricsCDKExample',
           statistic: Stats.percentile(50),
-          label: 'p50'
+          label: 'p50',
         }),
         new Metric({
           metricName: 'Latency',
           namespace: 'MomentoMetricsCDKExample',
           statistic: Stats.percentile(90),
-          label: 'p90'
+          label: 'p90',
         }),
         new Metric({
           metricName: 'Latency',
           namespace: 'MomentoMetricsCDKExample',
           statistic: Stats.percentile(99),
-          label: 'p99'
+          label: 'p99',
         }),
       ],
       title: 'Latency',
@@ -302,8 +290,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           statistic: Stats.percentile(50),
           label: 'p50 OK',
           dimensionsMap: {
-            'status': '0'
-          }
+            status: '0',
+          },
         }),
         new Metric({
           metricName: 'Latency + GRPC Status',
@@ -311,8 +299,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           statistic: Stats.percentile(99),
           label: 'p99 OK',
           dimensionsMap: {
-            'status': '0'
-          }
+            status: '0',
+          },
         }),
         new Metric({
           metricName: 'Latency + GRPC Status',
@@ -320,8 +308,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           statistic: Stats.percentile(50),
           label: 'p50 ERROR: INTERNAL',
           dimensionsMap: {
-            'status': '13'
-          }
+            status: '13',
+          },
         }),
         new Metric({
           metricName: 'Latency + GRPC Status',
@@ -329,8 +317,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           statistic: Stats.percentile(99),
           label: 'p99 ERROR: INTERNAL',
           dimensionsMap: {
-            'status': '13'
-          }
+            status: '13',
+          },
         }),
       ],
       title: 'Latency + Select GRPC Statuses',
@@ -349,8 +337,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           statistic: Stats.percentile(50),
           label: 'p50 GET',
           dimensionsMap: {
-            'requestType': '_GetRequest'
-          }
+            requestType: '_GetRequest',
+          },
         }),
         new Metric({
           metricName: 'Latency + Request Type',
@@ -358,8 +346,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           statistic: Stats.percentile(99),
           label: 'p99 GET',
           dimensionsMap: {
-            'requestType': '_GetRequest'
-          }
+            requestType: '_GetRequest',
+          },
         }),
         new Metric({
           metricName: 'Latency + Request Type',
@@ -367,8 +355,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           statistic: Stats.percentile(50),
           label: 'p50 SET',
           dimensionsMap: {
-            'requestType': '_SetRequest'
-          }
+            requestType: '_SetRequest',
+          },
         }),
         new Metric({
           metricName: 'Latency + Request Type',
@@ -376,8 +364,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           statistic: Stats.percentile(99),
           label: 'p99 SET',
           dimensionsMap: {
-            'requestType': '_SetRequest'
-          }
+            requestType: '_SetRequest',
+          },
         }),
       ],
       title: 'Latency + Select Request Types',
@@ -434,8 +422,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           label: 'p50 responses OK',
           statistic: Stats.percentile(50),
           dimensionsMap: {
-            'status': '0'
-          }
+            status: '0',
+          },
         }),
         new Metric({
           metricName: 'Response Size (bytes) + GRPC Status',
@@ -443,8 +431,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           label: 'p50 responses ERROR: INTERNAL',
           statistic: Stats.percentile(50),
           dimensionsMap: {
-            'status': '13'
-          }
+            status: '13',
+          },
         }),
         new Metric({
           metricName: 'Request Size (bytes) + GRPC Status',
@@ -452,8 +440,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           label: 'p50 requests OK',
           statistic: Stats.percentile(50),
           dimensionsMap: {
-            'status': '0'
-          }
+            status: '0',
+          },
         }),
         new Metric({
           metricName: 'Request Size (bytes) + GRPC Status',
@@ -461,8 +449,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           label: 'p50 requests ERROR: INTERNAL',
           statistic: Stats.percentile(50),
           dimensionsMap: {
-            'status': '13'
-          }
+            status: '13',
+          },
         }),
       ],
       title: 'Size in Bytes + Select GRPC Statuses',
@@ -481,8 +469,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           label: 'p50 responses GET',
           statistic: Stats.percentile(50),
           dimensionsMap: {
-            'requestType': '_GetRequest'
-          }
+            requestType: '_GetRequest',
+          },
         }),
         new Metric({
           metricName: 'Response Size (bytes) + Request Type',
@@ -490,8 +478,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           label: 'p50 responses SET',
           statistic: Stats.percentile(99),
           dimensionsMap: {
-            'requestType': '_SetRequest'
-          }
+            requestType: '_SetRequest',
+          },
         }),
         new Metric({
           metricName: 'Request Size (bytes) + Request Type',
@@ -499,8 +487,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           label: 'p50 requests GET',
           statistic: Stats.percentile(50),
           dimensionsMap: {
-            'requestType': '_GetRequest'
-          }
+            requestType: '_GetRequest',
+          },
         }),
         new Metric({
           metricName: 'Request Size (bytes) + Request Type',
@@ -508,8 +496,8 @@ export class MomentoMetricsStack extends cdk.Stack {
           label: 'p99 requests SET',
           statistic: Stats.percentile(99),
           dimensionsMap: {
-            'requestType': '_SetRequest'
-          }
+            requestType: '_SetRequest',
+          },
         }),
       ],
       title: 'Size in Bytes + Select Request Types',
@@ -526,7 +514,7 @@ export class MomentoMetricsStack extends cdk.Stack {
     const requestsPerSecond = new GraphWidget({
       left: [
         new MathExpression({
-          expression: 'countPerMinute / 60', 
+          expression: 'countPerMinute / 60',
           label: 'Average count of requests per second',
           usingMetrics: {
             countPerMinute: new Metric({
@@ -534,8 +522,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               namespace: 'MomentoMetricsCDKExample',
               statistic: Stats.SAMPLE_COUNT,
             }),
-          }
-        })
+          },
+        }),
       ],
       title: 'Number of requests per second',
       period: graphPeriod,
@@ -605,8 +593,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               label: 'p50 GET requests',
               statistic: Stats.percentile(50),
               dimensionsMap: {
-                'requestType': '_GetRequest'
-              }
+                requestType: '_GetRequest',
+              },
             }),
             m2: new Metric({
               metricName: 'Response Size (bytes) + Request Type',
@@ -614,8 +602,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               label: 'p50 GET responses',
               statistic: Stats.percentile(50),
               dimensionsMap: {
-                'requestType': '_GetRequest'
-              }
+                requestType: '_GetRequest',
+              },
             }),
           },
         }),
@@ -629,8 +617,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               label: 'p99 GET requests',
               statistic: Stats.percentile(99),
               dimensionsMap: {
-                'requestType': '_GetRequest'
-              }
+                requestType: '_GetRequest',
+              },
             }),
             m4: new Metric({
               metricName: 'Response Size (bytes) + Request Type',
@@ -638,8 +626,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               label: 'p99 GET responses',
               statistic: Stats.percentile(99),
               dimensionsMap: {
-                'requestType': '_GetRequest'
-              }
+                requestType: '_GetRequest',
+              },
             }),
           },
         }),
@@ -653,8 +641,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               label: 'p50 SET requests',
               statistic: Stats.percentile(50),
               dimensionsMap: {
-                'requestType': '_SetRequest'
-              }
+                requestType: '_SetRequest',
+              },
             }),
             m6: new Metric({
               metricName: 'Response Size (bytes) + Request Type',
@@ -662,8 +650,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               label: 'p50 SET responses',
               statistic: Stats.percentile(50),
               dimensionsMap: {
-                'requestType': '_SetRequest'
-              }
+                requestType: '_SetRequest',
+              },
             }),
           },
         }),
@@ -677,8 +665,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               label: 'p99 SET requests',
               statistic: Stats.percentile(99),
               dimensionsMap: {
-                'requestType': '_SetRequest'
-              }
+                requestType: '_SetRequest',
+              },
             }),
             m8: new Metric({
               metricName: 'Response Size (bytes) + Request Type',
@@ -686,8 +674,8 @@ export class MomentoMetricsStack extends cdk.Stack {
               label: 'p99 SET responses',
               statistic: Stats.percentile(99),
               dimensionsMap: {
-                'requestType': '_SetRequest'
-              }
+                requestType: '_SetRequest',
+              },
             }),
           },
         }),
@@ -712,13 +700,13 @@ export class MomentoMetricsStack extends cdk.Stack {
   }
 
   validateMomentoApiKey(key?: string) {
-    return (key && key.length > 0);
+    return key && key.length > 0;
   }
 
   validateLogGroupName(stackConfig: string, name?: string) {
-    if (stackConfig === "dashboard-only" && name && name.length > 0) {
+    if (stackConfig === 'dashboard-only' && name && name.length > 0) {
       return true;
-    } else if (validStackConfigs.includes(stackConfig) && stackConfig != "dashboard-only") {
+    } else if (validStackConfigs.includes(stackConfig) && stackConfig !== 'dashboard-only') {
       return true;
     }
     return false;
