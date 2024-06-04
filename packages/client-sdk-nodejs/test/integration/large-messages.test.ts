13fa789c374613ac71b139703ac8fecfb1f637f3
Anita Ruangrotsakun <138700973+anitarua@users.noreply.github.com>
2024-06-04 09:54:42 -0700

    chore: add more logs and safeguards around 5mb test (#1303)
    * chore: add more logs and safeguards around 5mb test
    
    * add itemGetTtl call after set
    
    * revert 1mb edit for local testing

--- packages/client-sdk-nodejs/test/integration/large-messages.test.ts
+++ packages/client-sdk-nodejs/test/integration/large-messages.test.ts
@@ -1,27 +1,43 @@
 import {expectWithMessage} from '@gomomento/common-integration-tests';
 import {SetupIntegrationTest} from './integration-setup';
-import {CacheGet, CacheSet} from '@gomomento/sdk-core';
+import {CacheGet, CacheItemGetTtl, CacheSet} from '@gomomento/sdk-core';
+import {v4} from 'uuid';
 
 const {cacheClient, integrationTestCacheName} = SetupIntegrationTest();
 
 describe('CacheClient', () => {
   it('should send and receive 5mb messages', async () => {
     const value = 'a'.repeat(5_000_000);
-    const key = '5mb';
+    const key = `js-5mb-key-${v4()}`;
+    const ttlSeconds = 2000; // 2000 seconds == 30 minutes
 
     const setResponse = await cacheClient.set(
       integrationTestCacheName,
       key,
-      value
+      value,
+      {ttl: ttlSeconds}
     );
     expectWithMessage(() => {
       expect(setResponse).toBeInstanceOf(CacheSet.Success);
-    }, `expected to successfully set 5mb string for key ${key}, received ${setResponse.toString()}`);
+    }, `[${new Date().toLocaleTimeString()}] expected to successfully set 5mb string for key ${key} with ttl ${ttlSeconds} seconds, received ${setResponse.toString()}`);
 
-    const getResponse = await cacheClient.get(integrationTestCacheName, '5mb');
+    const ttlResponse = await cacheClient.itemGetTtl(
+      integrationTestCacheName,
+      key
+    );
+    expectWithMessage(() => {
+      expect(ttlResponse).toBeInstanceOf(CacheItemGetTtl.Hit);
+    }, `expected to successfully get ttl for key ${key}, received ${ttlResponse.toString()}`);
+
+    const ttlValue = (ttlResponse as CacheItemGetTtl.Hit).remainingTtlMillis();
+    expectWithMessage(() => {
+      expect(ttlValue).toBePositive();
+    }, `expected ttl for key ${key} to be positive, received ${ttlValue}`);
+
+    const getResponse = await cacheClient.get(integrationTestCacheName, key);
     expectWithMessage(() => {
       expect(getResponse).toBeInstanceOf(CacheGet.Hit);
-    }, `expected to successfully get 5mb string for key ${key}, received ${getResponse.toString()}`);
+    }, `[${new Date().toLocaleTimeString()}] expected to successfully get 5mb string for key ${key}, received ${getResponse.toString()}`);
 
     const responseValue = (getResponse as CacheGet.Hit).value();
     expectWithMessage(() => {
