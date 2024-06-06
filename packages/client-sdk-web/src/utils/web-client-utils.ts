665aee233ba5286df1975d056a6eba03be740a12
rishtigupta <rishti@momentohq.com>
2024-06-06 14:32:10 -0700

    fix: correct convertToB64String function to correctly handle international characters

--- packages/client-sdk-web/src/utils/web-client-utils.ts
+++ packages/client-sdk-web/src/utils/web-client-utils.ts
@@ -2,11 +2,26 @@ import {CredentialProvider} from '@gomomento/sdk-core';
 
 export function convertToB64String(v: string | Uint8Array): string {
   if (typeof v === 'string') {
-    return btoa(v);
+    const encodedString = encodeURIComponent(v);
+    let decodedString = '';
+    for (let i = 0; i < encodedString.length; i++) {
+      if (encodedString[i] === '%') {
+        const hex = encodedString.substr(i + 1, 2);
+        decodedString += String.fromCharCode(parseInt(hex, 16));
+        i += 2;
+      } else {
+        decodedString += encodedString[i];
+      }
+    }
+    return btoa(decodedString);
+  } else {
+    const chars = new Uint8Array(v);
+    let binary = '';
+    for (let i = 0; i < chars.length; i++) {
+      binary += String.fromCharCode(chars[i]);
+    }
+    return btoa(binary);
   }
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  return btoa(String.fromCharCode.apply(null, v));
 }
 
 export function createCallMetadata(
