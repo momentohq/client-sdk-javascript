8363375b8c3bc4b2ed33ade685e4f6ca1aefabb9
rishtigupta <rishti@momentohq.com>
2024-06-07 12:26:04 -0700

    feat: migrate leaderboard responses to new enum pattern

--- packages/core/src/messages/responses/leaderboard/leaderboard-fetch.ts
+++ packages/core/src/messages/responses/leaderboard/leaderboard-fetch.ts
@@ -1,29 +1,18 @@
 import {SdkError} from '../../../errors';
 import {_RankedElement} from '../grpc-response-types';
-import {ResponseBase, ResponseError, ResponseSuccess} from '../response-base';
+import {BaseResponseError, BaseResponseSuccess} from '../response-base';
+import {LeaderboardFetchResponse} from '../enums';
+
+interface IResponse {
+  type: LeaderboardFetchResponse;
+}
 
 /**
- * Parent response type for a leaderboard fetch by rank or by score request.  The
- * response object is resolved to a type-safe object of one of
- * the following subtypes:
- *
- * - {Success}
- * - {Error}
- *
- * `instanceof` type guards can be used to operate on the appropriate subtype.
- * @example
- * For example:
- * ```
- * if (response instanceof LeaderboardFetchByScore.Error) {
- *   // Handle error as appropriate.  The compiler will smart-cast `response` to type
- *   // `LeaderboardFetchByScore.Error` in this block, so you will have access to the properties
- *   // of the Error class; e.g. `response.errorCode()`.
- * }
- * ```
+ * Indicates a Successful leaderboard fetch request.
  */
-export abstract class Response extends ResponseBase {}
-
-class _Success extends Response {
+export class Success extends BaseResponseSuccess implements IResponse {
+  readonly type: LeaderboardFetchResponse.Success =
+    LeaderboardFetchResponse.Success;
   private readonly _elements: _RankedElement[];
 
   constructor(elements: _RankedElement[]) {
@@ -47,17 +36,6 @@ class _Success extends Response {
 }
 
 /**
- * Indicates a Successful leaderboard fetch request.
- */
-export class Success extends ResponseSuccess(_Success) {}
-
-class _Error extends Response {
-  constructor(protected _innerException: SdkError) {
-    super();
-  }
-}
-
-/**
  * Indicates that an error occurred during the leaderboard fetch by rank or by score request.
  *
  * This response object includes the following fields that you can use to determine
@@ -67,4 +45,13 @@ class _Error extends Response {
  * - `message()` - a human-readable description of the error
  * - `innerException()` - the original error that caused the failure; can be re-thrown.
  */
-export class Error extends ResponseError(_Error) {}
+export class Error extends BaseResponseError implements IResponse {
+  readonly type: LeaderboardFetchResponse.Error =
+    LeaderboardFetchResponse.Error;
+
+  constructor(_innerException: SdkError) {
+    super(_innerException);
+  }
+}
+
+export type Response = Success | Error;
--- packages/core/src/messages/responses/leaderboard/leaderboard-length.ts
+++ packages/core/src/messages/responses/leaderboard/leaderboard-length.ts
@@ -1,29 +1,19 @@
 import {SdkError} from '../../../errors';
-import {ResponseBase, ResponseError, ResponseSuccess} from '../response-base';
+import {BaseResponseError, BaseResponseSuccess} from '../response-base';
+import {LeaderboardLengthResponse} from '../enums';
+
+interface IResponse {
+  type: LeaderboardLengthResponse;
+}
 
 /**
- * Parent response type for a leaderboard length request.  The
- * response object is resolved to a type-safe object of one of
- * the following subtypes:
- *
- * - {Success}
- * - {Error}
- *
- * `instanceof` type guards can be used to operate on the appropriate subtype.
- * @example
- * For example:
- * ```
- * if (response instanceof LeaderboardLength.Error) {
- *   // Handle error as appropriate.  The compiler will smart-cast `response` to type
- *   // `LeaderboardLength.Error` in this block, so you will have access to the properties
- *   // of the Error class; e.g. `response.errorCode()`.
- * }
- * ```
+ * Indicates a successful leaderboard length request.
  */
-export abstract class Response extends ResponseBase {}
-
-class _Success extends Response {
+export class Success extends BaseResponseSuccess implements IResponse {
+  readonly type: LeaderboardLengthResponse.Success =
+    LeaderboardLengthResponse.Success;
   private readonly _length: number;
+
   constructor(length: number) {
     super();
     this._length = length;
@@ -43,17 +33,6 @@ class _Success extends Response {
 }
 
 /**
- * Indicates a successful leaderboard length request.
- */
-export class Success extends ResponseSuccess(_Success) {}
-
-class _Error extends Response {
-  constructor(protected _innerException: SdkError) {
-    super();
-  }
-}
-
-/**
  * Indicates that an error occurred during the leaderboard length request.
  *
  * This response object includes the following fields that you can use to determine
@@ -63,4 +42,13 @@ class _Error extends Response {
  * - `message()` - a human-readable description of the error
  * - `innerException()` - the original error that caused the failure; can be re-thrown.
  */
-export class Error extends ResponseError(_Error) {}
+export class Error extends BaseResponseError implements IResponse {
+  readonly type: LeaderboardLengthResponse.Error =
+    LeaderboardLengthResponse.Error;
+
+  constructor(_innerException: SdkError) {
+    super(_innerException);
+  }
+}
+
+export type Response = Success | Error;
