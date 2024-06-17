f00103ef2aa9d1405107e44cd0e890ddde99eb21
rishtigupta <rishti@momentohq.com>
2024-06-17 15:41:32 -0700

    fix: consistent styling

--- examples/nodejs/aws/eventbridge/webapp/src/svgs/arrow-left.tsx
+++ examples/nodejs/aws/eventbridge/webapp/src/svgs/arrow-left.tsx
@@ -1,6 +1,6 @@
 export const ArrowLeft = () => {
   return (
-    <svg className="h-8 w-8 text-gray-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2"
+    <svg className="h-6 w-6 text-gray-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2"
          stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
       <path stroke="none" d="M0 0h24v24H0z"/>
       <line x1="5" y1="12" x2="19" y2="12"/>
