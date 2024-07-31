c4101860bd1af47853b58f936e1f8a67c70ced44
anitarua <anita@momentohq.com>
2024-07-31 15:54:31 -0700

    reset deadline only after last one has passed

--- packages/client-sdk-nodejs/src/internal/grpc/storage-client-timeout-interceptor.ts
+++ packages/client-sdk-nodejs/src/internal/grpc/storage-client-timeout-interceptor.ts
@@ -18,7 +18,8 @@ export function createStorageClientTimeoutInterceptor(
 export class StorageClientTimeoutInterceptor {
   private readonly logger: MomentoLogger;
   private readonly responseDataReceivedTimeoutMs: number;
-  private readonly overallDeadline: number;
+  private readonly overallRequestTimeoutMs: number;
+  private overallDeadline?: Date;
 
   constructor(
     loggerFactory: MomentoLoggerFactory,
@@ -27,41 +28,59 @@ export class StorageClientTimeoutInterceptor {
   ) {
     this.logger = loggerFactory.getLogger(this);
     this.responseDataReceivedTimeoutMs = responseDataReceivedTimeoutMs;
-    const newDate = new Date(Date.now());
-    this.overallDeadline = newDate.setMilliseconds(
-      newDate.getMilliseconds() + overallRequestTimeoutMs
-    );
-    this.logger.debug(
-      `creating interceptor, overall deadline: ${this.overallDeadline}`
-    );
+    this.overallRequestTimeoutMs = overallRequestTimeoutMs;
   }
 
   public createTimeoutInterceptor(): Interceptor {
     return (options, nextCall) => {
-      this.logger.debug(
-        `intercepting call with options.deadline ${
-          options.deadline?.valueOf() || 'undefined'
-        }`
-      );
-
-      const deadline = new Date(Date.now());
-      deadline.setMilliseconds(
-        deadline.getMilliseconds() + this.responseDataReceivedTimeoutMs
-      );
-
-      if (deadline.valueOf() > this.overallDeadline.valueOf()) {
-        this.logger.debug(
-          'Unable to successfully retry request within client timeout, canceling request'
+      // Replace default timeout with the desired overall timeout
+      // on the first time through and set the first incremental timeout
+      if (this.overallDeadline === undefined) {
+        const newDate = new Date(Date.now());
+        newDate.setMilliseconds(
+          newDate.getMilliseconds() + this.overallRequestTimeoutMs
         );
-        const call = new InterceptingCall(nextCall(options));
-        call.cancelWithStatus(
-          status.CANCELLED,
-          'Unable to successfully retry request within client timeout'
+        this.overallDeadline = newDate;
+
+        const deadline = new Date(Date.now());
+        deadline.setMilliseconds(
+          deadline.getMilliseconds() + this.responseDataReceivedTimeoutMs
         );
-        return call;
-      } else {
         options.deadline = deadline;
         this.logger.debug(`new deadline set to ${options.deadline.valueOf()}`);
+        return new InterceptingCall(nextCall(options));
+      }
+
+      const receivedDeadline = options.deadline?.valueOf() || 0;
+      this.logger.debug(
+        `intercepting call with options.deadline ${receivedDeadline}`
+      );
+
+      // Reset incremental deadline only if it has been reached
+      if (receivedDeadline < new Date(Date.now()).valueOf()) {
+        this.logger.debug('received deadline < current time, resetting');
+
+        const deadline = new Date(Date.now());
+        deadline.setMilliseconds(
+          deadline.getMilliseconds() + this.responseDataReceivedTimeoutMs
+        );
+
+        if (deadline.valueOf() > this.overallDeadline.valueOf()) {
+          this.logger.debug(
+            'Unable to successfully retry request within client timeout, canceling request'
+          );
+          const call = new InterceptingCall(nextCall(options));
+          call.cancelWithStatus(
+            status.CANCELLED,
+            'Unable to successfully retry request within client timeout'
+          );
+          return call;
+        } else {
+          options.deadline = deadline;
+          this.logger.debug(
+            `new deadline set to ${options.deadline.valueOf()}`
+          );
+        }
       }
 
       return new InterceptingCall(nextCall(options));
