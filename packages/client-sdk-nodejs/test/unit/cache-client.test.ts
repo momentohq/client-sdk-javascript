ef7018dd0992abd7fb9a37b0d4db337005c4a2be
Chris Price <cprice404@users.noreply.github.com>
2024-08-20 11:12:00 -0700

    fix: switch to a jest reporter that shows correct error counts, avoid network calls in unit tests (#1414)
    This commit just does a few housekeeping things.
    
    1. Switch from `jest-ci-spec-reporter` to `jest-spec-reporter`. They are similar,
       and neither of them is perfect, and neither of them has hardly any stars on github,
       but the `jest-spec-reporter` one prints the correct error counts at the end of the run.
       The `jest-ci-spec-reporter` one does not. (They both have the desired trait of using
       an output format that prints success/failure results for each test case on a single line,
       which makes it easier for operators to diagnose issues.
    2. Unit tests should not make network calls. Our cache client unit tests were doing so. This
       changes them to prevent them from doing that.

--- packages/client-sdk-nodejs/test/unit/cache-client.test.ts
+++ packages/client-sdk-nodejs/test/unit/cache-client.test.ts
@@ -5,11 +5,8 @@ import {
   SimpleCacheClient,
   CreateCache,
   StringMomentoTokenProvider,
-  CredentialProvider,
-  MomentoErrorCode,
 } from '../../src';
 import {SimpleCacheClientProps} from '../../src/cache-client-props';
-import {ConnectionError} from '@gomomento/sdk-core';
 
 // This auth token is syntactically correct but not actually valid; it won't work with the real Momento Servers.
 // Used only for unit testing the constructors etc.
@@ -32,14 +29,14 @@ describe('CacheClient', () => {
 
   it('cannot create/get cache with invalid name', async () => {
     const invalidCacheNames = ['', '    '];
-    const momento = await CacheClient.create({
+    const momento = new CacheClient({
       configuration: configuration,
-      credentialProvider:
-        CredentialProvider.fromEnvironmentVariable('MOMENTO_API_KEY'),
+      credentialProvider: credentialProvider,
       defaultTtlSeconds: 100,
     });
     for (const name of invalidCacheNames) {
       const createResponse = await momento.createCache(name);
+      console.error(`CREATE RESPONSE: ${createResponse.toString()}`);
       expect(createResponse).toBeInstanceOf(CreateCache.Error);
       if (createResponse instanceof CreateCache.Error) {
         expect(createResponse.innerException()).toBeInstanceOf(
@@ -48,12 +45,12 @@ describe('CacheClient', () => {
       }
     }
   });
-  it('cannot create a client with an invalid request timeout', async () => {
+  it('cannot create a client with an invalid request timeout', () => {
     try {
       const invalidTimeoutConfig = configuration.withTransportStrategy(
         configuration.getTransportStrategy().withClientTimeoutMillis(-1)
       );
-      await CacheClient.create({
+      new CacheClient({
         configuration: invalidTimeoutConfig,
         credentialProvider: credentialProvider,
         defaultTtlSeconds: 100,
@@ -65,29 +62,9 @@ describe('CacheClient', () => {
       }
     }
   });
-  it('createWithEagerConnection throws if it cannot connect', async () => {
+  it('cannot create a client with an invalid default TTL', () => {
     try {
-      await CacheClient.create({
-        configuration: configuration,
-        credentialProvider: credentialProvider,
-        defaultTtlSeconds: 100,
-      });
-      // If the function call above does not throw, explicitly fail the test.
-      expect('Expected error was not thrown').toBeUndefined();
-    } catch (e) {
-      if (e instanceof ConnectionError) {
-        // Now TypeScript knows 'e' is an Error, so 'message' is accessible.
-        expect(e._errorCode).toEqual(MomentoErrorCode.CONNECTION_ERROR);
-        expect(e.message).toContain('Unable to connect to Momento');
-      } else {
-        // Handle the case where 'e' is not an Error object.
-        expect('Error is not an instance of Error').toBeUndefined();
-      }
-    }
-  });
-  it('cannot create a client with an invalid default TTL', async () => {
-    try {
-      await CacheClient.create({
+      new CacheClient({
         configuration: configuration,
         credentialProvider: credentialProvider,
         defaultTtlSeconds: -100,
@@ -100,7 +77,7 @@ describe('CacheClient', () => {
     }
 
     try {
-      await CacheClient.create({
+      new CacheClient({
         configuration: configuration,
         credentialProvider: credentialProvider,
         defaultTtlSeconds: 10.5,
@@ -112,12 +89,12 @@ describe('CacheClient', () => {
       }
     }
   });
-  it('cannot create a client with concurrent requests limit below 1', async () => {
+  it('cannot create a client with concurrent requests limit below 1', () => {
     try {
       const negativeLimitConfig = configuration.withTransportStrategy(
         configuration.getTransportStrategy().withMaxConcurrentRequests(-1)
       );
-      await CacheClient.create({
+      new CacheClient({
         configuration: negativeLimitConfig,
         credentialProvider: credentialProvider,
         defaultTtlSeconds: 100,
@@ -133,7 +110,7 @@ describe('CacheClient', () => {
       const zeroLimitConfig = configuration.withTransportStrategy(
         configuration.getTransportStrategy().withMaxConcurrentRequests(0)
       );
-      await CacheClient.create({
+      new CacheClient({
         configuration: zeroLimitConfig,
         credentialProvider: credentialProvider,
         defaultTtlSeconds: 100,
