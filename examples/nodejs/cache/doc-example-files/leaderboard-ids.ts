7dcc8229c4171aa049ff7019da2b4796cfd55345
Chris Price <chris@momentohq.com>
2024-08-28 13:02:52 -0700

    chore: adding examples that are currently hard-coded in dev docs
    This commit copies over a lot of example JS code that is currently
    hard-coded in the dev docs, into example files here that we can
    validate in CI. Also hopefully makes it easier to make sweeping
    changes in the future (such as our instanceof migration).
    
    A subsequent dev docs PR will consume these and get rid of the
    hard-coded examples.

---
+++ examples/nodejs/cache/doc-example-files/leaderboard-ids.ts
@@ -0,0 +1,111 @@
+import {
+  PreviewLeaderboardClient,
+  LeaderboardConfigurations,
+  CredentialProvider,
+  CacheClient,
+  Configurations,
+  LeaderboardOrder,
+  CreateCacheResponse,
+  LeaderboardUpsertResponse,
+  LeaderboardFetchResponse,
+  LeaderboardDeleteResponse,
+} from '@gomomento/sdk';
+
+async function main() {
+  // 1. Create a CacheClient to connect to Momento
+  const cacheClient = await CacheClient.create({
+    configuration: Configurations.Laptop.v1(),
+    credentialProvider: CredentialProvider.fromEnvironmentVariable({
+      environmentVariableName: 'MOMENTO_API_KEY',
+    }),
+    defaultTtlSeconds: 60,
+  });
+
+  // 2. Create a cache for the leaderboard to live in
+  const createCacheResponse = await cacheClient.createCache('my-cache');
+  switch (createCacheResponse.type) {
+    case CreateCacheResponse.AlreadyExists:
+      console.log('cache already exists');
+      break;
+    case CreateCacheResponse.Success:
+      console.log('cache created');
+      break;
+    case CreateCacheResponse.Error:
+      console.log('cache already exists');
+  }
+
+  // 3. Create a Leaderboard client
+  const client = new PreviewLeaderboardClient({
+    configuration: LeaderboardConfigurations.Laptop.v1(),
+    credentialProvider: CredentialProvider.fromEnvironmentVariable({
+      environmentVariableName: 'MOMENTO_API_KEY',
+    }),
+  });
+
+  // 4. Create a leaderboard with given cache and leaderboard names
+  const leaderboard = client.leaderboard('my-cache', 'my-leaderboard');
+
+  // 5. Create a mapping between element IDs and arbitrary strings
+  const id2name: Record<number, string> = {
+    123: 'Alice',
+    456: 'Bob',
+    789: 'Charlie',
+    1234: 'David',
+    5678: 'Eve',
+  };
+
+  // 6. Upsert the elements along with some scores
+  const upsertElements: Record<number, number> = {};
+  for (const id of Object.keys(id2name)) {
+    upsertElements[Number(id)] = Math.floor(Math.random() * 1000);
+  }
+  const upsertResponse = await leaderboard.upsert(upsertElements);
+  switch (upsertResponse.type) {
+    case LeaderboardUpsertResponse.Success:
+      console.log('Upsert success!');
+      break;
+    case LeaderboardUpsertResponse.Error:
+      console.log('Upsert error:', upsertResponse.message());
+      break;
+  }
+
+  // 7. Fetch the top 5 elements in the leaderboard and print scores and corresponding strings
+  const fetchResponse = await leaderboard.fetchByRank(0, 5, {order: LeaderboardOrder.Descending});
+  switch (fetchResponse.type) {
+    case LeaderboardFetchResponse.Success: {
+      const elements = fetchResponse.values();
+      console.log('------------Top 5 Leaderboard-------------');
+      console.log('------------------------------------------');
+      for (const element of elements) {
+        console.log(`${id2name[element.id]}: ${element.score}`);
+      }
+      console.log('------------------------------------------');
+      break;
+    }
+    case LeaderboardFetchResponse.Error:
+      console.log('Fetch error:', fetchResponse.message());
+      break;
+  }
+
+  // 8. Delete the entire leaderboard when done.
+  // Leaderboard items have a default 7-day TTL so make sure to clean up
+  // all unnecessary elements when no longer needed.
+  const deleteResp = await leaderboard.delete();
+  switch (deleteResp.type) {
+    case LeaderboardDeleteResponse.Success:
+      console.log('Delete leaderboard success');
+      break;
+    case LeaderboardDeleteResponse.Error:
+      console.log('Delete leaderboard error:', deleteResp.message());
+      break;
+  }
+}
+
+main()
+  .then(() => {
+    console.log('Leaderboard example completed!');
+  })
+  .catch((e: Error) => {
+    console.error(`Uncaught exception while running example: ${e.message}`);
+    throw e;
+  });
---
+++ examples/nodejs/webhooks/doc-example-files/webhook-doc-examples.ts
@@ -0,0 +1,55 @@
+/**
+ *
+ * This file contains examples of consuming the JavaScript APIs, for use as examples
+ * in the public dev docs.  Each function name that begins with `example_` is available
+ * to the dev docs to inject into the code snippets widget for the specified API.
+ *
+ * These examples should all be JavaScript; we can add TypeScript-specific examples in
+ * a second file in the future if desired.
+ *
+ */
+import {
+  CredentialProvider,
+  TopicClient,
+  TopicConfigurations,
+  TopicItem,
+  TopicPublishResponse,
+} from '@gomomento/sdk';
+
+async function example_API_WebhookTopicSubscribe(topicClient: TopicClient, cacheName: string) {
+  const result = await topicClient.subscribe(cacheName, 'topic 2', {
+    onError: () => {
+      console.error('Received an error from the topic');
+      return;
+    },
+    onItem: (item: TopicItem) => {
+      console.log(`Received uppercase text on topic 'topic-b': ${item.value().toString()}`);
+      return;
+    },
+  });
+}
+
+async function example_API_WebhookTopicPublish(topicClient: TopicClient, cacheName: string) {
+  const publishResponse = await topicClient.publish(cacheName, 'topic 1', 'a value');
+  switch (publishResponse.type) {
+    case TopicPublishResponse.Success:
+      console.log('Value published successfully!');
+      break;
+    case TopicPublishResponse.Error:
+      console.log(`Error publishing value: ${publishResponse.toString()}`);
+      break;
+  }
+}
+
+async function main() {
+  const topicClient = new TopicClient({
+    configuration: TopicConfigurations.Default.latest(),
+    credentialProvider: CredentialProvider.fromEnvironmentVariable('MOMENTO_API_KEY'),
+  });
+  await example_API_WebhookTopicSubscribe(topicClient, 'cache');
+  await example_API_WebhookTopicPublish(topicClient, 'cache');
+}
+
+main().catch(e => {
+  throw e;
+});
