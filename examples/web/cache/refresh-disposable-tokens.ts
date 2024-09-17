73d7f8f16118240a84f433f5b4cc5a5ea9f91e9c
Anita Ruangrotsakun <138700973+anitarua@users.noreply.github.com>
2024-09-17 14:48:28 -0700

    feat: web sdk example with topic client that refreshes disposable tokens (#1437)
    Implements a wrapper class TokenRefreshingTopicClient that automatically refreshes disposable tokens and active topics subscriptions before tokens expire.
    
    A standalone example exists in a single file under examples/web/cache/refresh-disposable-tokens.ts and can be run completely locally by using a localTokenVendingMachine() method, or can be run against a deployed TVM by passing the endpoint to the example code.
    
    The existing Vite chat app was also updated to use the TokenRefreshingTopicClient. Also fixed some bugs with the TVM and web app setup while testing and updated the CSS to be more Momento-themed.

---
+++ examples/web/cache/refresh-disposable-tokens.ts
@@ -0,0 +1,250 @@
+import {
+  AllTopics,
+  AuthClient,
+  CredentialProvider,
+  DisposableTokenScopes,
+  ExpiresAt,
+  ExpiresIn,
+  GenerateDisposableTokenResponse,
+  TopicClient,
+  TopicItem,
+  TopicPublish,
+  TopicPublishResponse,
+  TopicSubscribe,
+  TopicSubscribeResponse,
+} from '@gomomento/sdk-web';
+import {initJSDom} from './utils/jsdom';
+
+// In your own setup, the token vending machine would likely be a separate service.
+// In this file, we provide a local version if you want to run this example completely independently.
+// We also provide a version that fetches a token from a deployed token vending machine.
+// See https://github.com/momentohq/client-sdk-javascript/tree/main/examples/nodejs/token-vending-machine
+// for more information about deploying your own token vending machine.
+
+async function localTokenVendingMachine(): Promise<{token: string; expiresAt: ExpiresAt}> {
+  const authClient = new AuthClient({});
+  const tokenResponse = await authClient.generateDisposableToken(
+    DisposableTokenScopes.topicPublishSubscribe('my-cache', AllTopics),
+    ExpiresIn.seconds(30)
+  );
+  if (tokenResponse.type === GenerateDisposableTokenResponse.Error) {
+    throw new Error(`Failed to generate a disposable token: ${tokenResponse.toString()}`);
+  }
+  const humanReadableTime = new Date(tokenResponse.expiresAt.epoch() * 1000).toISOString();
+  console.log(`Generated a disposable token that will expire at ${humanReadableTime}`);
+  const disposableToken = {
+    token: tokenResponse.authToken,
+    expiresAt: tokenResponse.expiresAt,
+  };
+  return disposableToken;
+}
+
+async function tokenVendingMachine(): Promise<{token: string; expiresAt: ExpiresAt}> {
+  const resp = await fetch(process.env.TVM_ENDPOINT as string);
+  const respJson = (await resp.json()) as {authToken: string; expiresAt: number};
+  const disposableToken = {
+    token: respJson.authToken,
+    expiresAt: ExpiresAt.fromEpoch(respJson.expiresAt),
+  };
+  return disposableToken;
+}
+
+interface TokenRefreshingTopicClientProps {
+  refreshBeforeExpiryMs: number;
+  getDisposableToken: () => Promise<{token: string; expiresAt: ExpiresAt}>;
+}
+
+class TokenRefreshingTopicClient {
+  topicClient: TopicClient;
+  refreshBeforeExpiryMs: number;
+  getDisposableToken: () => Promise<{token: string; expiresAt: ExpiresAt}>;
+  activeSubscriptions: Record<
+    string,
+    {
+      cacheName: string;
+      topicName: string;
+      lastSequenceNumber: number;
+      unsubscribe: () => void;
+      onItem: (item: TopicItem) => void;
+      onError: (error: TopicSubscribe.Error) => void;
+    }
+  > = {};
+
+  private constructor(props: TokenRefreshingTopicClientProps) {
+    this.refreshBeforeExpiryMs = props.refreshBeforeExpiryMs;
+    this.getDisposableToken = props.getDisposableToken;
+  }
+
+  private async initialize() {
+    const disposableToken = await this.getDisposableToken();
+    this.topicClient = new TopicClient({
+      credentialProvider: CredentialProvider.fromString(disposableToken.token),
+    });
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    setTimeout(async () => {
+      await this.refreshToken();
+    }, getRefreshAfterMs(disposableToken.expiresAt, this.refreshBeforeExpiryMs));
+    console.log('Initialized topic client and set first timeout');
+  }
+
+  static async create(props: TokenRefreshingTopicClientProps) {
+    const client = new TokenRefreshingTopicClient(props);
+    await client.initialize();
+    return client;
+  }
+
+  private async refreshToken() {
+    console.log('Disposable token expiring soon, refreshing topic client with new token');
+    const disposableToken = await this.getDisposableToken();
+    const newTopicClient = new TopicClient({
+      credentialProvider: CredentialProvider.fromString(disposableToken.token),
+    });
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    setTimeout(async () => {
+      await this.refreshToken();
+    }, getRefreshAfterMs(disposableToken.expiresAt, this.refreshBeforeExpiryMs));
+
+    // for each active subscription, unsubscribe from the old topic client and subscribe to the new one
+    for (const key in this.activeSubscriptions) {
+      const value = this.activeSubscriptions[key];
+      const newSubscription = await newTopicClient.subscribe(value.cacheName, value.topicName, {
+        onItem: value.onItem,
+        onError: value.onError,
+      });
+      value.unsubscribe();
+
+      if (newSubscription.type === TopicSubscribeResponse.Error) {
+        console.error(`Error subscribing to topic: ${newSubscription.toString()}`);
+      } else {
+        this.activeSubscriptions[key].unsubscribe = () => {
+          newSubscription.unsubscribe();
+        };
+      }
+    }
+    this.topicClient = newTopicClient;
+  }
+
+  async publish(cacheName: string, topicName: string, message: string, onError?: (resp: TopicPublish.Error) => void) {
+    const resp = await this.topicClient.publish(cacheName, topicName, message);
+    if (resp.type === TopicPublishResponse.Error) {
+      console.error(`Error publishing message: ${resp.toString()}`);
+      if (onError) {
+        onError(resp);
+      }
+    }
+  }
+
+  async subscribe(
+    cacheName: string,
+    topicName: string,
+    options: {
+      onItem: (item: TopicItem) => void;
+      onError: (error: TopicSubscribe.Error) => void;
+    }
+  ) {
+    console.log(`Subscribe function called for ${cacheName}:${topicName}`);
+
+    const wrappedOnItem = (item: TopicItem) => {
+      const currentSubscription = this.activeSubscriptions[`${cacheName}:${topicName}`];
+      // pass through to user-provided onItem only if message hasn't been processed before
+      if (item.sequenceNumber() > currentSubscription.lastSequenceNumber) {
+        options.onItem(item);
+        currentSubscription.lastSequenceNumber = item.sequenceNumber();
+      }
+    };
+
+    const resp = await this.topicClient.subscribe(cacheName, topicName, {
+      onItem: wrappedOnItem,
+      onError: options.onError,
+    });
+
+    if (resp.type === TopicSubscribeResponse.Error) {
+      console.error(`Error subscribing to topic: ${resp.toString()}`);
+    } else {
+      console.log(`Subscribed to ${cacheName}:${topicName}`);
+      const key = `${cacheName}:${topicName}`;
+
+      // if key already exists, update the existing subscription
+      // otherwise make new record
+      if (key in this.activeSubscriptions) {
+        this.activeSubscriptions[key].unsubscribe = () => {
+          resp.unsubscribe();
+        };
+      } else {
+        this.activeSubscriptions[key] = {
+          cacheName,
+          topicName,
+          lastSequenceNumber: 0,
+          unsubscribe: () => {
+            resp.unsubscribe();
+          },
+          onItem: wrappedOnItem,
+          onError: options.onError,
+        };
+      }
+      console.log('New subscription added, active subscriptions are now:', this.activeSubscriptions);
+    }
+  }
+}
+
+function getRefreshAfterMs(expiresAt: ExpiresAt, refreshBefore: number): number {
+  const refreshingIn = expiresAt.epoch() * 1000 - Date.now() - refreshBefore;
+  console.log(`Refreshing in ${refreshingIn} ms`);
+  return refreshingIn;
+}
+
+const main = async () => {
+  // Because the Momento Web SDK is intended for use in a browser, we use the JSDom library
+  // to set up an environment that will allow us to use it in a node.js program.
+  initJSDom();
+
+  // Expecting uninterrupted sequence number progression from each subscription.
+  const onItemA = (item: TopicItem) => {
+    console.log(`Callback A: User code processing message ${item.sequenceNumber()}`);
+  };
+  const onItemB = (item: TopicItem) => {
+    console.log(`Callback B: User code processing message ${item.sequenceNumber()}`);
+  };
+  const onError = (error: TopicSubscribe.Error) => {
+    console.error(`User code received error: ${error.toString()}`);
+  };
+  const onPublishError = (resp: TopicPublish.Error) => {
+    console.error(`User code received error while publishing message: ${resp.toString()}`);
+  };
+
+  const wrappedTopicClient = await TokenRefreshingTopicClient.create({
+    refreshBeforeExpiryMs: 10_000, // 10 seconds before token expires, refresh it.
+    getDisposableToken: localTokenVendingMachine,
+  });
+
+  await wrappedTopicClient.subscribe('my-cache', 'topic-1', {
+    onItem: onItemA,
+    onError,
+  });
+
+  await wrappedTopicClient.subscribe('my-cache', 'topic-2', {
+    onItem: onItemB,
+    onError,
+  });
+
+  const endDemoTime = Date.now() + 45_000; // Run for 45 seconds
+
+  // Meanwhile, publish messages and see how the topic client
+  // is refreshed after tokens expire every 20 seconds.
+  while (Date.now() < endDemoTime) {
+    await wrappedTopicClient.publish('my-cache', 'topic-1', 'Message for topic 1', onPublishError);
+    await wrappedTopicClient.publish('my-cache', 'topic-2', 'Message for topic 2', onPublishError);
+  }
+};
+
+main()
+  .then(() => {
+    console.log('End of demo!');
+    // Don't leave the process hanging
+    // eslint-disable-next-line no-process-exit
+    process.exit(0);
+  })
+  .catch((e: Error) => {
+    console.error(`Uncaught exception while running disposable tokens example: ${e.message}`);
+    throw e;
+  });
--- examples/web/vite-chat-app/src/components/chat-room.tsx
+++ examples/web/vite-chat-app/src/components/chat-room.tsx
@@ -2,11 +2,8 @@ import { useEffect, useRef, useState } from "react";
 
 import {
   type ChatEvent,
-  clearCurrentClient,
   EventTypes,
-  sendMessage,
-  subscribeToTopic,
-  userJoined,
+  MomentoWebClient,
 } from "../utils/momento-web";
 import { type TopicItem, type TopicSubscribe } from "@gomomento/sdk-web";
 
@@ -22,31 +19,31 @@ export default function ChatRoom(props: Props) {
   const [chats, setChats] = useState<ChatEvent[]>([]);
   const [textInput, setTextInput] = useState("");
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const [momentoClient, setMomentoClient] = useState<MomentoWebClient|undefined>(undefined);
 
   const onItem = (item: TopicItem) => {
     try {
       const message = JSON.parse(item.valueString()) as ChatEvent;
       setChats((curr) => [...curr, message]);
     } catch (e) {
-      console.error("unable to parse chat message", e);
+      // Messages received from elsewhere (like the Momento Console or an SDK)
+      // should be converted from TopicItem to ChatEvent to be readable on the UI.
+      const chatEvent: ChatEvent = {
+        event: EventTypes.MESSAGE,
+        text: item.valueString(),
+        username: item.tokenId() || "unknown",
+        timestamp: Date.now(),
+      };
+      setChats((curr) => [...curr, chatEvent]);
     }
   };
 
-  const onError = async (
-    error: TopicSubscribe.Error,
-    sub: TopicSubscribe.Subscription,
-  ) => {
-    console.error(
-      "received error from momento, getting new token and resubscribing",
-      error,
-    );
-    sub.unsubscribe();
-    clearCurrentClient();
-    await subscribeToTopic(props.cacheName, props.topicName, onItem, onError);
-  };
+  const onError = (error: TopicSubscribe.Error) => {
+    console.error("received error from momento", error);
+  }
 
   const onSendMessage = async () => {
-    await sendMessage(
+    await momentoClient?.sendMessage(
       props.cacheName,
       props.topicName,
       props.username,
@@ -62,13 +59,22 @@ export default function ChatRoom(props: Props) {
   };
 
   useEffect(() => {
-    subscribeToTopic(props.cacheName, props.topicName, onItem, onError, props.selectedUser)
-      .then(async () => {
-        console.log("successfully subscribed");
-        await userJoined(props.cacheName, props.topicName, props.username);
-      })
-      .catch((e) => console.error("error subscribing to topic", e));
-  }, []);
+    // Initializing the Momento TopicClient is an async operation because
+    // we first need to get a disposable token from the token vending machine.
+    // We must also pass in props.selectedUser here in case the cognito authorizer
+    // is used in the getDisposableToken function passed to the TokenRefreshingTopicClient.
+
+    const initialSetup = async (client: MomentoWebClient) => {
+      setMomentoClient(client);
+      await client.subscribeToTopic(props.cacheName, props.topicName, onItem, onError, props.selectedUser);
+      console.log("Successfully initialized momento client and subscribed to topic", props.topicName);
+      await momentoClient?.userJoined(props.cacheName, props.topicName, props.username);
+    }
+
+    MomentoWebClient.create(props.selectedUser)
+      .then(initialSetup)
+      .catch((e) => console.error("error initializing momento client", e));
+  }, [props.topicName, props.username, props.selectedUser]);
 
   const scrollToBottom = () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -81,13 +87,17 @@ export default function ChatRoom(props: Props) {
   }, [chats]);
 
   return (
-    <div className={"flex flex-col p-6 h-full justify-between bg-slate-200"}>
+    // <div className={"flex flex-col p-6 h-full justify-between bg-slate-200"}>
+    <div 
+      className={"flex flex-col p-6 h-full justify-between"}
+      style={{ background: "radial-gradient(circle, #25392B, #0E2515)" }}
+    >
       <div
         className={
           "flex justify-between items-center p-1 border-b-2 border-slate-300 mb-4"
         }
       >
-        <div>
+        <div className={"text-white"}>
           Welcome to the <span className={"italic"}>{props.topicName}</span>{" "}
           chat room
         </div>
@@ -103,26 +113,28 @@ export default function ChatRoom(props: Props) {
           const date = new Date(chat.timestamp);
           const hours = date.getHours();
           const minutes = date.getMinutes();
+          const timestampWithUsername = `[${hours}:${minutes}] <${chat.username}>`;
+          const timestamp = `[${hours}:${minutes}]`;
           switch (chat.event) {
             case EventTypes.MESSAGE:
-              const timestampWithUsername = `[${hours}:${minutes}] <${chat.username}>`;
               return (
                 <div
                   className={"break-words"}
                   key={`${chat.timestamp}-${chat.username}`}
                 >
-                  <span className={"text-red-500"}>
+                  <span className={"text-green-500"}>
                     {timestampWithUsername}
                   </span>{" "}
-                  {chat.text}
+                  <span className={"text-white"}>
+                    {chat.text}
+                  </span>
                 </div>
               );
             case EventTypes.USER_JOINED:
-              const timestamp = `[${hours}:${minutes}]`;
               return (
                 <div
                   key={`${chat.timestamp}-${chat.username}`}
-                  className={"text-green-500 italic"}
+                  className={"text-gray-400 italic"}
                 >
                   {timestamp} user joined: {chat.username}
                 </div>
@@ -135,6 +147,7 @@ export default function ChatRoom(props: Props) {
         <input
           disabled={props.selectedUser === "ReadOnly"}
           placeholder={"chat"}
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
           onKeyDown={onEnterClicked}
           className={"border-2 rounded-2xl p-2 w-full"}
           value={textInput}
@@ -142,10 +155,11 @@ export default function ChatRoom(props: Props) {
         />
         <div className={"w-4"} />
         <button
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
           onClick={onSendMessage}
           disabled={!textInput}
           className={
-            "bg-blue-400 rounded-2xl p-2 hover:brightness-75 disabled:bg-slate-400 disabled:brightness-75 disabled:text-white"
+            "bg-green-400 rounded-2xl p-2 hover:brightness-75 disabled:bg-slate-400 disabled:brightness-75 disabled:text-white"
           }
         >
           Send
