70981d834ab5d3e84126927761b03f13d76ae5bb
Michael Landis <michael@momentohq.com>
2024-10-14 11:44:27 -0700

    refactor: extract trial function from promise creation
    Previously we had a separate function in the inner loop of promise
    creation. Instead there is a single trial function that is
    parameterized by the trial number.
    
    We refactor the test to pull out the trial function and build the
    promise in the loop.

--- packages/common-integration-tests/src/cache/replica-reads.ts
+++ packages/common-integration-tests/src/cache/replica-reads.ts
@@ -15,43 +15,44 @@ export function runReplicaReadTests(
       const replicationDelayMs = 1000;
       const trials = [];
 
+      const trialFn = async (trialNumber: number) => {
+        // Start this trial at it's own time plus a random delay
+        const startDelay =
+          trialNumber * delayBetweenTrials + (Math.random() - 0.5) * 10;
+        await new Promise(resolve => setTimeout(resolve, startDelay));
+
+        const cacheKey = v4();
+        const cacheValue = v4();
+
+        // Perform a set operation
+        const setResponse = await client.set(
+          integrationTestCacheName,
+          cacheKey,
+          cacheValue
+        );
+        expectWithMessage(() => {
+          expect(setResponse).toBeInstanceOf(CacheSet.Success);
+        }, `expected SUCCESS but got ${setResponse.toString()}`);
+
+        // Wait for replication to complete
+        await new Promise(resolve => setTimeout(resolve, replicationDelayMs));
+
+        // Verify that the value can be read
+        const getResponse = await client.get(
+          integrationTestCacheName,
+          cacheKey
+        );
+        expectWithMessage(() => {
+          expect(getResponse).toBeInstanceOf(CacheGet.Hit);
+        }, `expected HIT but got ${getResponse.toString()}`);
+
+        expectWithMessage(() => {
+          expect(getResponse.value()).toEqual(cacheValue);
+        }, `expected ${cacheValue} but got ${getResponse.value() ?? 'undefined'}`);
+      };
+
       for (let i = 0; i < numTrials; i++) {
-        const trialPromise = (async () => {
-          // Start this trial at it's own time plus a random delay
-          const startDelay =
-            i * delayBetweenTrials + (Math.random() - 0.5) * 10;
-          await new Promise(resolve => setTimeout(resolve, startDelay));
-
-          const cacheKey = v4();
-          const cacheValue = v4();
-
-          // Perform a set operation
-          const setResponse = await client.set(
-            integrationTestCacheName,
-            cacheKey,
-            cacheValue
-          );
-          expectWithMessage(() => {
-            expect(setResponse).toBeInstanceOf(CacheSet.Success);
-          }, `expected SUCCESS but got ${setResponse.toString()}`);
-
-          // Wait for replication to complete
-          await new Promise(resolve => setTimeout(resolve, replicationDelayMs));
-
-          // Verify that the value can be read
-          const getResponse = await client.get(
-            integrationTestCacheName,
-            cacheKey
-          );
-          expectWithMessage(() => {
-            expect(getResponse).toBeInstanceOf(CacheGet.Hit);
-          }, `expected HIT but got ${getResponse.toString()}`);
-
-          expectWithMessage(() => {
-            expect(getResponse.value()).toEqual(cacheValue);
-          }, `expected ${cacheValue} but got ${getResponse.value() ?? 'undefined'}`);
-        })();
-        trials.push(trialPromise);
+        trials.push(trialFn(i));
       }
 
       await Promise.all(trials);
