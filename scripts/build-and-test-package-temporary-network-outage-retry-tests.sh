41761dc9ea9762f5148845df454f1b0ff4a7680d
rishtigupta <rishti@momentohq.com>
2025-01-21 15:12:41 -0800

    feat: add automated retry tests to ci

---
+++ scripts/build-and-test-package-temporary-network-outage-retry-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+set -e
+
+ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/.. &> /dev/null && pwd )
+
+echo "Building and testing package: ${1}"
+
+# Start the Docker container in the background
+DOCKER_CONTAINER_NAME="momento-local-test"
+docker run --name ${DOCKER_CONTAINER_NAME} -d -p 8080:8080 gomomento/momento-local \
+    --return-error unavailable --error-rpcs get --error-count 2
+
+# Ensure the container is stopped when the script exits
+function cleanup {
+    echo "Stopping and removing Docker container..."
+    docker stop ${DOCKER_CONTAINER_NAME}
+    docker rm ${DOCKER_CONTAINER_NAME}
+}
+trap cleanup EXIT
+
+${ROOT_DIR}/scripts/build-package.sh ${1}
+
+pushd ${ROOT_DIR}/packages/${1}
+    npm run integration-test-retry-temporary-network-outage
+popd
