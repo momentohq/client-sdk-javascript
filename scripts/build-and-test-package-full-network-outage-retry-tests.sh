3cb7b317e9d68e9d8b219c4c795cecad0fde1a66
rishtigupta <rishti@momentohq.com>
2025-01-22 12:50:14 -0800

    fix: partial cleanup

--- scripts/build-and-test-package-full-network-outage-retry-tests.sh
+++ scripts/build-and-test-package-full-network-outage-retry-tests.sh
@@ -9,13 +9,11 @@ echo "building and testing JS SDK with full network outage"
 
 ${ROOT_DIR}/scripts/build-package.sh "core"
 ${ROOT_DIR}/scripts/build-package.sh "common-integration-tests"
+${ROOT_DIR}/scripts/build-package.sh "client-sdk-nodejs"
 
 echo "building and testing retry with full network outage"
 echo "Target URL for tests: ${TEST_URL}"
 
-${ROOT_DIR}/scripts/build-package.sh ${1}
-
-pushd ${ROOT_DIR}/packages/${1}
-    npm run unit-test
+pushd ${ROOT_DIR}/packages/client-sdk-nodejs
     TEST_URL=${TEST_URL} npm run integration-test-retry-full-network-outage
 popd
