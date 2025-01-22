7598b7f0483e4c870adba90c5f150070fdead365
rishtigupta <rishti@momentohq.com>
2025-01-22 12:35:11 -0800

    chore: explicitly add perms

--- scripts/build-and-test-package-retry.sh
+++ scripts/build-and-test-package-retry.sh
@@ -7,6 +7,6 @@ ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/.. &> /dev/null && pwd )
 
 echo "building and testing retry with full network outage"
 
-${ROOT_DIR}/scripts/build-and-test-package-consistent-reads.sh "core"
+${ROOT_DIR}/scripts/build-package.sh "core"
 ${ROOT_DIR}/scripts/build-and-test-package-full-network-outage-retry-tests.sh "client-sdk-nodejs"
 #${ROOT_DIR}/scripts/build-and-test-package-temporary-network-outage-retry-tests.sh "client-sdk-nodejs"
