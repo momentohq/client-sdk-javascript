92d9f3b4e4180949869b436a7709eefa0f7b6653
rishtigupta <rishti@momentohq.com>
2025-01-22 16:04:30 -0800

    chore: address comment feedback

--- scripts/build-and-test-package-full-network-outage-retry-tests.sh
+++ scripts/build-and-test-package-full-network-outage-retry-tests.sh
@@ -7,8 +7,6 @@ ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/.. &> /dev/null && pwd )
 
 echo "building and testing JS SDK with full network outage"
 
-${ROOT_DIR}/scripts/build-package.sh "core"
-${ROOT_DIR}/scripts/build-package.sh "common-integration-tests"
 ${ROOT_DIR}/scripts/build-package.sh "client-sdk-nodejs"
 
 echo "building and testing retry with full network outage"
