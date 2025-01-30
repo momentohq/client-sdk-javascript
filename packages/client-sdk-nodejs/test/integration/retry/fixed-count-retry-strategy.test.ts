398857f7e6008547512476f84d33dcbd07f972a2
Michael Landis <michael@momentohq.com>
2025-01-29 16:49:45 -0800

    feat: add an exponential backoff with retry strategy (#1515)
    Adds an exponential backoff with retry strategy. This strategy takes
    into account an initial delay, a backoff schedule, and the previous
    backoff.
    
    In this PR we also organize the integration tests by retry strategy.

---
+++ packages/client-sdk-nodejs/test/integration/retry/fixed-count-retry-strategy.test.ts
@@ -0,0 +1,119 @@
+import {
+  CacheGetResponse,
+  CacheIncrementResponse,
+  DefaultMomentoLoggerFactory,
+  MomentoLogger,
+} from '../../../src';
+import {TestRetryMetricsCollector} from '../../test-retry-metrics-collector';
+import {
+  MomentoRPCMethod,
+  MomentoRPCMethodConverter,
+} from '../../../src/config/retry/momento-rpc-method';
+import {WithCacheAndCacheClient} from '../integration-setup';
+import {TestRetryMetricsMiddlewareArgs} from '../../test-retry-metrics-middleware';
+import {v4} from 'uuid';
+
+describe('Fixed count retry strategy with full network outage', () => {
+  let testMetricsCollector: TestRetryMetricsCollector;
+  let momentoLogger: MomentoLogger;
+
+  beforeAll(() => {
+    testMetricsCollector = new TestRetryMetricsCollector();
+    momentoLogger = new DefaultMomentoLoggerFactory().getLogger(
+      'TestRetryMetricsMiddleware'
+    );
+  });
+
+  it('should make max 3 attempts for retry eligible api for fixed count strategy', async () => {
+    const testMiddlewareArgs: TestRetryMetricsMiddlewareArgs = {
+      logger: momentoLogger,
+      testMetricsCollector: testMetricsCollector,
+      requestId: v4(),
+      returnError: 'unavailable',
+      errorRpcList: [MomentoRPCMethodConverter.convert(MomentoRPCMethod.Get)],
+    };
+
+    await WithCacheAndCacheClient(
+      config => config,
+      testMiddlewareArgs,
+      async (cacheClient, cacheName) => {
+        const getResponse = await cacheClient.get(cacheName, 'key');
+        expect(getResponse.type).toEqual(CacheGetResponse.Error);
+        if (getResponse.type === CacheGetResponse.Error) {
+          expect(getResponse.errorCode()).toEqual('SERVER_UNAVAILABLE');
+        }
+        const noOfRetries = testMetricsCollector.getTotalRetryCount(
+          cacheName,
+          MomentoRPCMethod.Get
+        );
+        expect(noOfRetries).toBe(3);
+      }
+    );
+  });
+
+  it('should make 0 attempts for retry non-eligible api for fixed count strategy', async () => {
+    const testMiddlewareArgs: TestRetryMetricsMiddlewareArgs = {
+      logger: momentoLogger,
+      testMetricsCollector: testMetricsCollector,
+      requestId: v4(),
+      returnError: 'unavailable',
+      errorRpcList: [
+        MomentoRPCMethodConverter.convert(MomentoRPCMethod.Increment),
+      ],
+    };
+    await WithCacheAndCacheClient(
+      config => config,
+      testMiddlewareArgs,
+      async (cacheClient, cacheName) => {
+        const incrementResponse = await cacheClient.increment(
+          cacheName,
+          'key',
+          1
+        );
+        expect(incrementResponse.type).toEqual(CacheIncrementResponse.Error);
+        const noOfRetries = testMetricsCollector.getTotalRetryCount(
+          cacheName,
+          MomentoRPCMethod.Increment
+        );
+        expect(noOfRetries).toBe(0); // Increment is not eligible for retry
+      }
+    );
+  });
+});
+
+describe('Fixed count retry strategy with temporary network outage', () => {
+  let testMetricsCollector: TestRetryMetricsCollector;
+  let momentoLogger: MomentoLogger;
+
+  beforeAll(() => {
+    testMetricsCollector = new TestRetryMetricsCollector();
+    momentoLogger = new DefaultMomentoLoggerFactory().getLogger(
+      'TestRetryMetricsMiddleware'
+    );
+  });
+
+  it('should make less than max number of allowed retry attempts for fixed count strategy', async () => {
+    const testMiddlewareArgs: TestRetryMetricsMiddlewareArgs = {
+      logger: momentoLogger,
+      testMetricsCollector: testMetricsCollector,
+      requestId: v4(),
+      returnError: 'unavailable',
+      errorRpcList: [MomentoRPCMethodConverter.convert(MomentoRPCMethod.Get)],
+      errorCount: 2,
+    };
+    await WithCacheAndCacheClient(
+      config => config,
+      testMiddlewareArgs,
+      async (cacheClient, cacheName) => {
+        const getResponse = await cacheClient.get(cacheName, 'key');
+        expect(getResponse.type).toEqual(CacheGetResponse.Miss); // Miss because we never set the key
+        const noOfRetries = testMetricsCollector.getTotalRetryCount(
+          cacheName,
+          MomentoRPCMethod.Get
+        );
+        expect(noOfRetries).toBeGreaterThan(1);
+        expect(noOfRetries).toBeLessThanOrEqual(3);
+      }
+    );
+  });
+});
